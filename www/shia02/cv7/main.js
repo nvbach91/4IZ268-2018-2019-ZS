32aa97fffd1029ece2ac260cf40eacce9ccbb1b1
Anastasia Shikhlyarova <shia02@vse.cz>
2018-11-02 12:31:50 +0100

    cv 7 JS

---
+++ www/shia02/cv7/main.js
@@ -0,0 +1,107 @@
+alert('hello world 3')
+
+// ultimate comment
+
+/*
+miltimate
+
+comment
+*/
+
+// kazdy radek je statement, po nich se pise strednik
+
+// Boolean
+var a = true;
+var b = false;
+
+// Number
+var c = 42;
+var d = 13.025464;
+
+// String
+var e = '42';
+var f = 'ahoj světe';
+var g = "ahoj světe";
+
+'<div class="class"></div>';
+
+// undefined
+var h = undefined;
+
+// null
+var i = null;
+
+// variables
+var j = 'jjj';  //logolovani, da se zmenit
+let k = 'k';  //lokalni, da se zmenit
+const pi = 3.14;  //neda se zmenit
+j = 'abc';
+k = 15;
+//pi = 10; CHYBA
+
+// functions
+var lambda = function () {
+    alert('funkce l zavolana');
+};
+// lambda(); //volani funkce
+
+var addUp = function (a, b) {
+    var sum = a+b;
+    return sum;
+};
+// addUp(5, 10);
+
+//arrays
+var p = [1, 2, 3, 4, 5];
+var q = ['ananas', 'banana'];
+
+// p[0]
+
+// objects
+var human = {
+    age: 42,
+    name: 'Bob',
+    kids: ['Charles', 'Anna'],
+    isBald: false,
+};
+
+var dave = {
+    name: 'David',
+    age: 59,
+    job: 'beggar',
+    money: 0,
+    achievements: [],
+};
+
+var myFirstPiano = {};
+
+// && - and, vsechno
+// || - or, aspon jedno z
+
+/*
+ if (podminka) {
+    to co se musi stat
+ } else {
+    to co se musi stat
+ } */
+
+ /*
+  for (var i = 0; i < 10; i++){
+     console.log(i);
+  }
+  */
+
+/*
+  var i = 0;
+  while(i < 10){
+      console.log(i);
+      i++;
+  }
+  */
+
+  /*
+var dog = {name: 'Dog', age:2, bark: function()
+{console.log('huff!')}};
+
+q.forEach(function (fruit))
+  */
