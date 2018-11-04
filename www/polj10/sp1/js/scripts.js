0c9b21e219c7c55664a23423a8b51600281a64db
JiriPol <jiri.polacek@outlook.com>
2018-11-05 00:11:30 +0100

    omgsemestralkanenicasnamezery

---
+++ www/polj10/sp1/js/scripts.js
@@ -0,0 +1,95 @@
+
+
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+
+    var x, y, i, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    for (i = 0; i < y.length; i++) {
+      if (elmnt == y[i]) {
+        arrNo.push(i)
+      } else {
+        y[i].classList.remove("select-arrow-active");
+      }
+    }
+    for (i = 0; i < x.length; i++) {
+      if (arrNo.indexOf(i)) {
+        x[i].classList.add("select-hide");
+      }
+    }
+  }
+
+
+
+function closeMobileNav(element) {
+  var navButton = document.getElementById("mobile-nav-button");
+  var nav = document.getElementById("navigation");
+  var header = document.getElementsByTagName("header")[0];
+  document.getElementsByTagName("main")[0].classList.remove("fixed-position");
+  document.getElementsByTagName("main")[0].classList.remove("top-3em")
+  document.getElementsByTagName("footer")[0].classList.remove("invisible");
+  nav.style.left = '-100vw';
+  header.style.position = 'relative';
+
+  mobileNavButtonAnimateBw();
+
+  navButton.addEventListener("click", openMobileNav);
+}
+
+function openMobileNav(element) {
+  var navButton = document.getElementById("mobile-nav-button");
+  var nav = document.getElementById("navigation");
+  var header = document.getElementsByTagName("header")[0];
+  document.getElementsByTagName("main")[0].classList.add("fixed-position");
+  document.getElementsByTagName("main")[0].classList.add("top-3em")
+  document.getElementsByTagName("footer")[0].classList.add("invisible");
+
+  nav.style.left = '0%';
+  header.style.position = 'fixed';
+
+  mobileNavButtonAnimateFw();
+
+  navButton.removeEventListener("click", openMobileNav);
+  navButton.addEventListener("click", closeMobileNav);
+}
+
+function mobileNavButtonAnimateBw() {
+  var topMenuBar = document.getElementById("top-menu-bar");
+  var midMenuBar1 = document.getElementById("mid-menu-bar1");
+  var midMenuBar2 = document.getElementById("mid-menu-bar2");
+  var bottomMenuBar = document.getElementById("bottom-menu-bar");
+
+  topMenuBar.classList.remove("top-menu-bar-animation");
+  midMenuBar1.classList.remove("mid-menu-bar1-animation");
+  midMenuBar2.classList.remove("mid-menu-bar2-animation");
+  bottomMenuBar.classList.remove("bottom-menu-bar-animation");
+
+  topMenuBar.classList.add("bw-top-menu-bar-animation");
+  midMenuBar1.classList.add("bw-mid-menu-bar1-animation");
+  midMenuBar2.classList.add("bw-mid-menu-bar2-animation");
+  bottomMenuBar.classList.add("bw-bottom-menu-bar-animation");
+  
+}
+
+function mobileNavButtonAnimateFw() {
+  var topMenuBar = document.getElementById("top-menu-bar");
+  var midMenuBar1 = document.getElementById("mid-menu-bar1");
+  var midMenuBar2 = document.getElementById("mid-menu-bar2");
+  var bottomMenuBar = document.getElementById("bottom-menu-bar");
+
+  topMenuBar.classList.remove("bw-top-menu-bar-animation");
+  midMenuBar1.classList.remove("bw-mid-menu-bar1-animation");
+  midMenuBar2.classList.remove("bw-mid-menu-bar2-animation");
+  bottomMenuBar.classList.remove("bw-bottom-menu-bar-animation");
+
+  topMenuBar.classList.add("top-menu-bar-animation");
+  midMenuBar1.classList.add("mid-menu-bar1-animation");
+  midMenuBar2.classList.add("mid-menu-bar2-animation");
+  bottomMenuBar.classList.add("bottom-menu-bar-animation");
+}
+
+
+
+
