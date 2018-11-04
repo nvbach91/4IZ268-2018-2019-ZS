1a74eee6427042384aa28309b0de5967c1e6fc8d
Tomáš Bušek <bust01@vse.cz>
2018-11-04 17:16:18 +0100

    Update tisk

---
+++ www/bust01/homework-05/js/exercs.js
@@ -0,0 +1,7 @@
+/*Pepe's age. Vypište na konzoli smysluplnou oznamovací větu ohledně
+věku Pepy, pokud znáte jeho rok narození. Použijte proměnné a pro výpis
+ použijte zřetězení stringů. Jako názvy proměnných používejte anglické pojmy.*/
+ var birthYear = 1999;
+ var currentYear = 2018;
+ var age = currentYear-birthYear;
+ console.log("Pepův věk je " + age + " let");
