30be523cb8b72a0e34cd411550033da2759a5aa2
Tomáš Bušek <bust01@vse.cz>
2018-11-07 20:41:54 +0100

    Homework-05

--- www/bust01/homework-05/js/exercs.js
+++ www/bust01/homework-05/js/exercs.js
@@ -12,38 +12,154 @@ věku Pepy, pokud znáte jeho rok narození. Použijte proměnné a pro výpis
     a. z C na F: vynásobit devíti, vydělit pěti a přičíst 32.
     b. z F na C: odečíst 32, vynásobit pěti a vydělit devítkou.*/
 var a = 20;
-var vypocet = (9*a)/5 + 32;
-console.log(a + "°C = " + vypocet + "°F");
+var result = (9*a)/5 + 32;
+console.log(a + "°C = " + result + "°F");
 
 var b = 68;
-var vypocet = ((b-32)*5)/9;
-console.log(b + "°F = " + vypocet + "°C");
+var result = ((b-32)*5)/9;
+console.log(b + "°F = " + result + "°C");
 
 /*Funkce function fonction funktio. Vemte předchozí úlohy a udělejte z nich
 funkce. Tj. vytvořte funkce, které přijímají argumenty, a na základě argumentů
 po zavolání vypíše výsledek na konzoli. Párkrát zavolejte tyto funkce s různými
 argumenty. V konzoli také vyzkoušejte, zda fungují vaše funkce.*/
+
 //Pepův věk
 var countAge = function(currentYear, birthYear) {
     var age = currentYear-birthYear;
  console.log("Pepův věk je " + age + " let");
 }
+
 //převod C a F
-var prevod = function(a, b)
+var transform = function(a, unit)
  {
-     var jednotka = ['C','F'];
-     var x = jednotka.shift(0);
-     var y = jednotka.shift(1);
-if (b == x) {
-    var vypocet = (9*a)/5 + 32;
-    console.log(a + "°" + jednotka + "=" + vypocet + "°F")
+     var temp = ['C','F'];
+     var x = temp.shift(0);
+     var y = temp.shift(1);
+if (unit == x) {
+    var result = (9*a)/5 + 32;
+    console.log(a + "°" + temp + "=" + result + "°F");
 }
-else { if (b == y) {
-    var vypocet = ((a-32)*5)/9;
-    console.log(a + "°" + jednotka + "=" + vypocet + "°C")
+else { if (unit == y) {
+    var result = ((a-32)*5)/9;
+    console.log(a + "°" + temp + "=" + result + "°C");
 }
 else {
     console.log("error");
 }
 }
 }
+
+/*%CENSORED%. Vytvořte funkci, která vezme 2 číselné 
+argumenty a vrátí podíl prvního čísla a druhého 
+čísla v procentech. Výsledek vypište do 
+konzole, např. 21 je 50% z 42. Pro zkrácení / zaokrouhlování 
+desetinných míst použijte funkci .toFixed(n). 
+Např. var pi = 3.1415926535; pi.toFixed(2); Pozor na dělení nulou!*/
+var division = function(a,b)
+{
+    if (b != 0) {
+        var result = (a/b)*100;
+        result.toFixed(2);
+        console.log(a + " je " + result + "% z " + b);
+    }
+    else {
+        console.log("Dělení nulou!")
+    }
+}
+
+/*Kdo s koho. Vytvořte funkci, která vezme 2 číselné
+argumenty a vrátí ten větší z nich. Pokud se čísla 
+rovnají, vypište, že se rovnají. Vyzkoušejte funkčnost 
+pro celá čísla, desetinná čísla, zlomky. Zkuste je párkrát 
+zavolat v kódu a výsledky uložit do proměnných.*/
+var comparison = function(a,b)
+{
+    var x;
+    if(a == b) {
+        console.log(a + " se rovná " + b);
+    }
+    else if (a > b) {
+        console.log(a);
+        x = a;
+    }
+    else {
+        console.log(b);
+        x=b;
+    }
+}
+
+/*I can cleary see the pattern. Vytvořte funkci, která
+vypíše popořadě všechny násobky 13, které jsou menší 
+nebo rovno 730. Použijte for loop.*/
+var pattern = function() {   
+var a = 13;
+    var i;
+    for (i = 1; ; i++) { 
+        var b = a*i;
+        if (b<730) {
+        console.log(b);
+        }
+        else {
+            break;
+        }
+    }
+}
+
+/*Around and about. Vytvořte funkci, která vypočte obsah
+kružnice podle dodaného poloměru.*/
+var area = function(r) {
+    var r;
+    var pi = 3.14;
+    result = pi * r * r;
+    console.log(result);
+}
+
+/*Another dimension. Vytvořte funkci, která vypočte objem
+kuželu, pokud znáte jeho výšku a poloměr.*/
+var capacity = function(r,v) {
+    var pi = 3.14;
+    result = 1/3*(pi*r*r*v);
+    console.log(result);
+}
+
+/*Not sure if triangle, or just some random values. Vytvořte
+funkci, která rozhodne, zda se z dodaných 3 délek dá postavit
+trojúhelník, tj. vypíše buď true/yes nebo false/no.*/
+var triangle = function (a,b,c) {
+var decision = new Boolean(true);
+var x = a+b;
+var y = a+c;
+var z = b+c;
+if (x>c && y>b && z>a)
+{console.log(decision);
+}
+else {
+    decision = Boolean(false);
+    console.log(decision);
+}
+}
+
+/*Heroic performance. Vytvořte funkci, která vypočte obsah 
+trojúhelníka podle Heronova vzorce, tj. funkce dostane 
+délky všech 3 stran. Použijte přitom předchozí validaci, tj. počítejte 
+pouze, když to má smysl. Hint: funkce pro odmocninu je Math.sqrt()*/
+var heron = function (a,b,c) {
+        var decision = new Boolean(true);
+        var x = a+b;
+        var y = a+c;
+        var z = b+c;
+        var s = ((a+b+c)/2);
+        if (x>c && y>b && z>a)
+        {
+            decision;
+            var S = s*((s-a)*(s-b)*(s-c));
+            var result = Math.sqrt(S);
+            console.log(S);
+            
+        }
+        else {
+            decision = false;
+            console.log("Nelze sestrojit trojúhelník");
+        }
+    }                                        
