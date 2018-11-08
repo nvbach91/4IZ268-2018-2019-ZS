8c20973f09798fcd3f587e10c1a2fe2aeaaea419
Nguyen Viet Bach <nvbach91@outlook.com>
2018-11-09 00:07:48 +0100

    added pokemon project

---
+++ www/nguv03/cv08-pokemons/js/main.js
@@ -0,0 +1,86 @@
+var pokemonForm = document.querySelector('#pokemon-form');
+var addButton = document.querySelector('#add-button');
+var pokemonInput = document.querySelector('#pokemon-name');
+var pokemonList = document.querySelector('#pokemon-list');
+var selectedPokemon = document.querySelector('#selected-pokemon');
+
+var createNewPokemon = function(name) {
+  /*
+    <li class="pokemon">
+      <div class="pokemon-name">newPokemonName</div>
+      <button class="pokemon-delete">Delete</div>
+    </li>
+  */
+  var pokemon = document.createElement('li');
+  pokemon.classList.add('pokemon');
+
+  var pokemonName = document.createElement('div');
+  pokemonName.classList.add('pokemon-name');
+  pokemonName.innerText = name;
+  pokemonName.addEventListener('click', function() {
+    var existingPokemons = document.querySelectorAll('.pokemon');
+    existingPokemons.forEach(function(existingPokemon) {
+      existingPokemon.classList.remove('selected');
+    });
+    this.classList.toggle('selected');
+    selectedPokemon.innerText = name + ', I choose you!';
+  });
+
+  var deleteButton = document.createElement('button');
+  deleteButton.classList.add('pokemon-delete');
+  deleteButton.innerText = 'Bye';
+  deleteButton.addEventListener('click', function() {
+    this.parentNode.parentNode.removeChild(pokemon);
+    selectedPokemon.innerText = 'Bye bye, ' + name + '!';
+  });
+
+  pokemon.appendChild(pokemonName);
+  pokemon.appendChild(deleteButton);
+
+  return pokemon;
+};
+
+var addPokemon = function() {
+  var newPokemonName = pokemonInput.value;
+
+  // checking if the input is empty
+  if (!newPokemonName) {
+    alert('Please enter a pokemon name');
+    return false;
+  }
+
+  // checking if the pokemon is already in the list
+  var existingPokemonNames = document.querySelectorAll('.pokemon-name');
+  for (var i = 0; i < existingPokemonNames.length; i++) {
+    var existingPokemonName = existingPokemonNames[i].innerText;
+    if (newPokemonName === existingPokemonName) {
+      alert('Pokemon ' + newPokemonName + ' already exists!');
+      return false;
+    }
+  }
+
+  // create the pokemon item
+  var newPokemon = createNewPokemon(newPokemonName);
+
+  // append it to the list
+  pokemonList.appendChild(newPokemon);
+
+  // reset the input
+  pokemonInput.value = '';
+};
+
+pokemonForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  addPokemon();
+});
+
+addButton.addEventListener('click', addPokemon);
+
+pokemonInput.value = 'Pikachu';
+addPokemon();
+pokemonInput.value = 'Charmander';
+addPokemon();
+pokemonInput.value = 'Bulbasaur';
+addPokemon();
+pokemonInput.value = 'Squirtle';
+addPokemon();
