14a97d324591a042a47e9059b21b5f95aa625934
Jakub Voráček <jakub.voracek@gmail.com>
2018-11-26 14:12:44 +0100

    make selected, playing and active cards

--- www/vorj00/ukol_07/assets/js/main.js
+++ www/vorj00/ukol_07/assets/js/main.js
@@ -1,28 +1,55 @@
 var points = 0;
-var moves = 0;
 var cities = ['Barcelona', 'Dortmund', 'Madrid', 'Turin', 'Prague', 'New York', 'Paris', 'London', 'Beijing', 'Moscow'];
-
-const pexesoBoard = document.querySelector('#js-pexeso');
+var firstSelect = null;
 
 cities = cities.concat(cities);
 cities.sort(function() { return .5 - Math.random(); });
 
-cities.forEach(city => {
-    let elem = document.createElement('div');
-    elem.className = 'pexeso__card';
-    elem.innerHTML = city;
-
-    pexesoBoard.appendChild(elem);
-})
-
-const pexesoCard = document.querySelectorAll('.pexeso__card')
-
-pexesoCard.addEventListener('click', () => {
-    if(moves < 2){
-        moves++;
+function editPoints(change){
+    if((change > 0) || (points > 0)){
+        points+=change;
     } else {
-        moves = 0;
+        points = 0;
     }
+}
 
-    console.log(moves)
-})
+$(document).ready(function(){
+    const pexesoPoints = '#js-points';
+    const pexesoBoard = '#js-pexeso';
+    const pexesoCard = '.js-playable';
+
+    $(pexesoPoints).text(points);
+
+    $.each(cities, function(index, value){
+        $(pexesoBoard).append(`<div class="pexeso__card pexeso__card--playing js-playable">${value}</div>`)
+    });
+
+    $(pexesoCard).on("click", function(){
+        $(this).addClass('pexeso__card--selected').removeClass('pexeso__card--playing').removeClass('js-playable');
+
+        let currentCard = $(this);
+
+        if(firstSelect === null){
+            firstSelect = currentCard;
+        } else {
+            if(firstSelect.text() === currentCard.text()){
+                firstSelect.addClass('pexeso__card--active');
+                currentCard.addClass('pexeso__card--active');
+
+                editPoints(1);
+            } else {
+                firstSelect.addClass('pexeso__card--playing');
+                currentCard.addClass('pexeso__card--playing');
+
+                editPoints(-1);
+            }
+
+            firstSelect.removeClass('pexeso__card--selected');
+            currentCard.removeClass('pexeso__card--selected');
+
+            firstSelect = null;
+        }
+
+        $(pexesoPoints).text(points);
+    });
+});
