24d1d376185a9140a7a8795d1e17afcc19ababe0
Nguyen Viet Bach <nvbach91@outlook.com>
2018-11-30 18:59:22 +0100

    fix hw8

--- www/nguv03/homework-08/task/js/app.js
+++ www/nguv03/homework-08/task/js/app.js
@@ -6,7 +6,7 @@ var App = App || {};
 // INSERT CLIENT ID FROM GITHUB
 App.client_id = '...';
 // INSERT CLIENT SECRET FROM GITHUB
-App.client_secret = '';
+App.client_secret = '...';
 App.baseApiUrl = 'https://api.github.com';
 App.renderUser = function(user) {
   
