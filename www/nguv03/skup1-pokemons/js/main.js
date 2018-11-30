6de9337ab12f35a48b581df0ae61cb6a358e0aeb
Nguyen Viet Bach <nvbach91@outlook.com>
2018-11-30 13:38:05 +0100

    added both pokemon groups

--- www/nguv03/skup1-pokemons/js/main.js
+++ www/nguv03/skup1-pokemons/js/main.js
@@ -5,16 +5,16 @@
 
 
 var pokemonForm = document.querySelector('#pokemon-form');
-var pokemonNameInput = document.querySelector('#name-input');
+var pokemonNameInput = document.querySelector('#pokemon-name-input');
 var pokemonList = document.querySelector('#pokemon-list');
-var pokemonCall = document.querySelector('#pokemon-call');
+var pokemonCall = document.querySelector('#pokemon-text');
 var checkPokemons = function (name) {
     // check if pokemon already exists
     var existingPokemons = document.querySelectorAll('.pokemon-name');
     for (var i = 0; i < existingPokemons.length; i++) {
         var pokemon = existingPokemons[i];
         //console.log(pokemon.innerText);
-        if (name.toLowerCase().trim() === pokemon.innerHTML.toLowerCase().trim()) {
+        if (name.toLowerCase().trim() === pokemon.innerText.toLowerCase().trim()) {
             return true;
         }
     }
@@ -26,13 +26,29 @@ var createPokemon = function (pokemonName) {
 
     var pokemonImage = document.createElement('div');
     pokemonImage.classList.add('pokemon-image');
-    pokemonImage.style.backgroundImage = 'url(https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/' + pokemonName + '.png)';
+    pokemonImage.style.backgroundImage = 'url(https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/' + pokemonName.toLowerCase() + '.png)';
 
     var newPokemonName = document.createElement('div');
     newPokemonName.classList.add('pokemon-name');
     newPokemonName.innerText = pokemonName;
     newPokemonName.addEventListener('click', function () {
         pokemonCall.innerText = pokemonName + ', I choose you! :]';
+    });
+
+    var newPokemonWeight = document.createElement('div');
+    newPokemonWeight.classList.add('pokemon-weight');
+    
+    var loader = document.createElement('div');
+    loader.classList.add('loader');
+
+    newPokemon.appendChild(loader);
+    newPokemon.appendChild(newPokemonWeight);
+
+    fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName.toLowerCase() + '/').then(function (resp) {
+        return resp.json();
+    }).then(function (resp) {
+        newPokemon.removeChild(loader);
+        newPokemonWeight.innerText = resp.weight;
     });
 
     var newPokemonDelete = document.createElement('button');
@@ -51,13 +67,14 @@ var createPokemon = function (pokemonName) {
 };
 pokemonForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    var pokemonName = pokemonNameInput.value;
-    var pokemonAlreadyExists = checkPokemons(pokemonName);
-    if (pokemonAlreadyExists) {
-        alert('Pokemon already exists!');
-        return false;
-    }
-    addPokemonToList(pokemonName);
+    pokemonNameInput.value.split(',').forEach(function (pokemonName) {
+        var pokemonAlreadyExists = checkPokemons(pokemonName);
+        if (pokemonAlreadyExists) {
+            alert('Pokemon already exists!');
+            return false;
+        }
+        addPokemonToList(pokemonName);
+    });
 });
 var addPokemonToList = function (pokemonName) {
     var newPokemon = createPokemon(pokemonName);
@@ -65,7 +82,6 @@ var addPokemonToList = function (pokemonName) {
 };
 
 
-var pokemonImage = 'https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/' + pokemonName + '.png';
 
 
 
