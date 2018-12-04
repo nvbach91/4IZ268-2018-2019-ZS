13729e6eff51d7851ed86bfb3433dadc3506392d
kukd01 <kuki.david@gmail.com>
2018-12-04 21:56:10 +0100

    informations

--- www/kukd01/homework-08/js/app.js
+++ www/kukd01/homework-08/js/app.js
@@ -20,6 +20,21 @@ $.getJSON(url).done(function (user) {
 });*/
 
 App.renderUser = function (user) {
+  App.html = `
+    <div class="info">
+      <div class="info login"> Login :${user.login}</div>
+      <div class="info bio"> Company :${user.company || " "}</div>
+      <div class="info location"> Location :${user.location || " "}</div>
+      <div class="info bio"> Bio :${user.bio || " "}</div>
+      <div class="info email"> Email :${user.email || " "}</div>
+      <div class="info followers"> Followers :${user.followers || " "}</div>
+      <div class="info registred"> Registred :${user.created_at || " "}</div >
+  <div class="info url"> <a href = "${user.html_url || " "}">${user.html_url || " "}</div>
+    </div >
+  `;
+
+  App.userProfile.html(App.html);
+
 
 };
 
@@ -34,13 +49,14 @@ App.init = function () {
 
   App.formButton.submit(function (e) {
     e.preventDefault();
+    var searchValue = App.searchValue.val(); //Proč tady musím použít tuto funkci a nemohu vzít rovnou hodnotu ze selectoru - searchValue ? 
     if (!App.searchValue) { return false; }
 
-    var url = App.baseApiUrl + '/users/' + App.searchValue + '?client_id=' + App.client_id + '&client_secret=' + App.client_secret;
+    var url = App.baseApiUrl + '/users/' + searchValue + '?client_id=' + App.client_id + '&client_secret=' + App.client_secret;
     App.userProfile.empty();
-
+    //Tohle musí volat asi až v Ajax done ? 
     $.getJSON(url).done(function (user) {
-      renderUser(user);
+      App.renderUser(user);
       // fetchRepositories(user.login);
     }).fail(function () {
       $('#user-profile').html('<p>User not found</p>');
