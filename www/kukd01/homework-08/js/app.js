731f658ef316b657b5c1d0f980ab0e18eead56b2
kukd01 <kuki.david@gmail.com>
2018-12-06 22:49:43 +0100

    nevrací počet repozitářů jinak už za mě hotovo

--- www/kukd01/homework-08/js/app.js
+++ www/kukd01/homework-08/js/app.js
@@ -8,10 +8,10 @@ App.client_id = '248e90c1e8e75a696626';
 // INSERT CLIENT SECRET FROM GITHUB
 App.client_secret = 'ac7652120d5103e5ea1a3612aeec262644006cb4';
 App.baseApiUrl = 'https://api.github.com';
-App.searchValue = $('#search-input');
+
 
 App.renderUser = function (user) {
-  App.html = `
+  var html = `
     <div class="info">
       <div class="avatar" style="background-image: url(${user.avatar_url})"> </div>
       <div class="info login"> Login :${user.login}</div>
@@ -25,50 +25,58 @@ App.renderUser = function (user) {
     </div >
   `;
 
-  App.userProfile.html(App.html);
+  App.userProfile.html(html);
 
 };
 
-//Jak si tady mám dosadit login ? Myslel jsem když v initu spouštím
-//tuto funkci tak že se místo loginu dosadí daný parametr z prvního JSONU
-App.fetchRepositories = function (login) {
+
+App.fetchRepositories = function (a) {
+
+  var login = App.searchInput.val();
   var urlrep = App.baseApiUrl + '/users/' + login + '/repos';
   App.repositories.empty();
 
-  $.getJSON(urlrep).done(function (user) {
-    App.fetchRepositories(user.login);
-  })
+  $.getJSON(urlrep).done(function (repositories) {
+    var repos = `<p>This user has ${repositories.lenght || " "} repositories</p>`;
+
+    repositories.forEach(function (repository) {
+      repos +=
+        `<li class ="repo"> 
+        <div class ="repo-name">${repository.name}</div>
+        <div class ="repo-url"><a href="${repository.html_url}">${repository.html_url}</a></div>
+      </li>
+    `;
+    });
 
 
-
+    App.repositories.html(repos);
+  });
 
 
 };
-
-
 
 
 
 
 App.init = function () {
-  App.searchValue = $('#search-input');
-  App.formButton = $('#search-form');
+  App.searchInput = $('#search-input');
+  App.searchForm = $('#search-form');
   App.userProfile = $('#user-profile');
   App.repositories = $('#repositories');
 
-  App.formButton.submit(function (e) {
+  App.searchForm.submit(function (e) {
     e.preventDefault();
-    var searchValue = App.searchValue.val(); //Proč tady musím použít tuto funkci a nemohu vzít rovnou hodnotu ze selectoru - searchValue ? 
-    if (!App.searchValue) { return false; }
+    var searchInput = App.searchInput.val();
+    if (!App.searchInput) { return false; }
 
-    var url = App.baseApiUrl + '/users/' + searchValue + '?client_id=' + App.client_id + '&client_secret=' + App.client_secret;
+    var url = App.baseApiUrl + '/users/' + searchInput + '?client_id=' + App.client_id + '&client_secret=' + App.client_secret;
     App.userProfile.empty();
 
     $.getJSON(url).done(function (user) {
       App.renderUser(user);
       App.fetchRepositories(user.login);
     }).fail(function () {
-      $('#user-profile').html('<p>User not found</p>');
+      App.userProfile.html('<p>User not found</p>');
     });
 
 
