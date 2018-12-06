ff67df2f137a5843022b9c96741001d7893738fb
Petra Vokálová <Blondie@petra-macbook-air.home>
2018-12-06 14:58:06 +0100

    button s aktuální polohou

--- www/vokp00/semestralka2/js/main.js
+++ www/vokp00/semestralka2/js/main.js
@@ -1,51 +1,62 @@
-function showLocation(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+/* pozice uživatele na mapě dle IP */
+var latitude = null;
+var longitude = null;
+var city = "";
 
-}
-
-function errorHandler(err) {
-    if (err.code == 1) {
-        alert("Error: Access is denied!");
-    } else if (err.code == 2) {
-        alert("Error: Position is unavailable!");
-    }
-}
-
-function getLocationUpdate() {
-
-    if (navigator.geolocation) {
-        var options = { timeout: 60000 };
-        var geoLoc = navigator.geolocation;
-        var watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
-        console.log(watchID);
-    } else {
-        alert("Sorry, browser does not support geolocation!");
-    }
-}
+fetch("http://ip-api.com/json/?fields=lat,lon,city").then(function (take) {
+    return take.json();
+}).then(function (take) {
+    latitude = take.lat;
+    longitude = take.lon;
+    city = take.city;
+});
 
 
-if (navigator.geolocation == true) {
-    var center = SMap.Coords.fromWGS84(position.coords.longitude, position.coords.latitude);
-} else {
-
-    var center = SMap.Coords.fromWGS84(14.4378005, 50.0755381);
-}
-
-/*var center = SMap.Coords.fromWGS84(14.4378005, 50.0755381);*/
-
+var center = SMap.Coords.fromWGS84(latitude, longitude);
 var map = new SMap(JAK.gel("map"), center, 10);
+var cz = map.computeCenterZoom(center);
+
+/* vaše pozice */
+
+var moreInfo = function () {
+
+    var buttonclicked;
+    $("#coors").click(function () {
+        if (buttonclicked != true) {
+            buttonclicked = true;
+            var para = document.createElement("P");
+            var t = document.createTextNode("You are somewhere near the coordinates " + latitude + ", " + longitude + " which means you are in " + city);
+            para.appendChild(t);
+            document.getElementById("coordinates").appendChild(para);
+        } else {
+            alert("Button was clicked before");
+        }
+    });
+}
 
 
+/* pozice uživatele v mapě
+var userPosition = new SMap.Layer.Marker();
+map.addLayer(userPosition);
+userPosition.enable();
 
+var picture = "https://img.icons8.com/nolan/2x/street-view.png";
+
+var options = {
+    url: picture,
+    anchor: { left: 10, bottom: 1 }  /* Ukotvení značky za bod uprostřed dole */
+/* } */
+
+/* var userMArker = new SMap.Marker(center, null, options);
+userPosition.addMarker(userMArker); */
 
 /* Aby mapa reagovala na změnu velikosti průhledu */
 map.addControl(new SMap.Control.Sync());
 
-/* Turistický podklad */
+/* Turistický podklad*/
 map.addDefaultLayer(SMap.DEF_TURIST).enable();
 
-/* Ovládání myší */
+/* Ovládání myší*/
 var mouse = new SMap.Control.Mouse(SMap.MOUSE_PAN | SMap.MOUSE_WHEEL | SMap.MOUSE_ZOOM);
 map.addControl(mouse);
 
