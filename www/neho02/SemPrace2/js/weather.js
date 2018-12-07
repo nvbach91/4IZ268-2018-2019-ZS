e71bbec0a18d70484ba622296174f81fb3ca347f
neho02 <iplesha7@gmail.com>
2018-12-07 19:08:17 +0100

    Finalni upravy

--- www/neho02/SemPrace2/js/weather.js
+++ www/neho02/SemPrace2/js/weather.js
@@ -12,7 +12,11 @@ $('#submitWeather').click(function(){
         type: "GET",
         dataType: "jsonp",
         success: function(data){
-            console.log(data);
+            var widget = show(data);
+
+            $("#show").html(widget);
+
+            $("#city").val(' ');
         }
 
     });
@@ -22,6 +26,12 @@ $('#submitWeather').click(function(){
     }
 
 });
-
-
 });
+
+function show(data){
+    return  "<h2>Current Weather for "+ data.name + "," + data.sys.country +"</h2>" +
+           "<h3><strong>Weather</strong>: <img src='http://openweathermap.org/img/w/" + data.weather[0].icon+".png'>"+ data.weather[0].main +"</h3>" +
+           "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
+           "<h3><strong>Temperature</strong>: "+ data.main.temp + "&deg;C</h3>";
+
+}
