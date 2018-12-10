724d64045036820fad35518aa484c7cb166f8153
Tomáš Bušek <bust01@vse.cz>
2018-12-10 15:55:41 +0100

    Oprava HW8

--- www/bust01/hw8/js/app.js
+++ www/bust01/hw8/js/app.js
@@ -8,8 +8,8 @@ App.client_id = 'b829208bba619271c3c6';
 // INSERT CLIENT SECRET FROM GITHUB
 App.client_secret = '7e38404ca4ef780e0e45da5dd78c8b912a22ae30';
 App.baseApiUrl = 'https://api.github.com';
-App.renderUser = function(user) {
-    var infoDet = `<div class="info">
+App.renderUser = function (user) {
+  var infoDet = `<div class="info">
         <div class="info-name">${user.name || ''}</div>
         <div class="info-main">
           <div class="avatar" style="background-image: url(${user.avatar_url})"></div>
@@ -26,9 +26,9 @@ App.renderUser = function(user) {
         </div>
         <div class="info-odkaz"><a href="${user.html_url || ''}" target="_blank">View profile</a></div>
       </div>`;
-    App.userProfile.html(infoDet);
-  }; 
-App.fetchRepositories = function(username) {
+  App.userProfile.html(infoDet);
+};
+App.fetchRepositories = function (username) {
   App.repositoriesField.empty();
   App.repositoriesField.append(App.loader);
   var url = App.baseApiUrl + '/users/' + username + '/repos';
@@ -39,16 +39,16 @@ App.fetchRepositories = function(username) {
       client_id: App.client_id,
       client_secret: App.client_secret,
     },
-  })
-  ajxRep.done(function(repositories) {
+  });
+  ajxRep.done(function (repositories) {
     App.loader.remove();
-    if(repositories.length == 1){
-    var html = `<p>This user has ${repositories.length} repository</p>`;
+    if (repositories.length == 1) {
+      var html = `<p>This user has ${repositories.length} repository</p>`;
     }
     else {
       var html = `<p>This user has ${repositories.length} repositories</p>`;
     }
-    repositories.forEach(function(repository) {
+    repositories.forEach(function (repository) {
       html += `<li class="repository">
     <div class="name">${repository.name}</div>
     <div class="url"><a href="${repository.html_url}">${repository.html_url}</a></div>
@@ -57,8 +57,8 @@ App.fetchRepositories = function(username) {
     App.repositoriesField.empty().append(html);
   })
 };
-App.init = function() {
-  App.searchForm.submit(function(e) {
+App.init = function () {
+  App.searchForm.submit(function (e) {
     e.preventDefault();
     var searchValue = App.searchInput.val();
     App.userProfile.append(App.loader);
@@ -68,20 +68,20 @@ App.init = function() {
       return false;
     }
     var url = App.baseApiUrl + '/users/' + searchValue + '?client_id=' + App.client_id + '&client_secret=' + App.client_secret;
-  var ajxUser = $.getJSON(url);
-  ajxUser.done(function(user) {
-   App.renderUser(user);
-   App.fetchRepositories(user.login);
-  })
-  ajxUser.fail(function() {
-  var html = `<div>User not found</div>`;
-  App.userProfile.html(html);
-  var htmlrep = `<div>No repositories</div>`;
-  App.repositoriesField.empty().html(htmlrep);
+    var ajxUser = $.getJSON(url);
+    ajxUser.done(function (user) {
+      App.renderUser(user);
+      App.fetchRepositories(user.login);
+    })
+    ajxUser.fail(function () {
+      var html = `<div>User not found</div>`;
+      App.userProfile.html(html);
+      var htmlrep = `<div>No repositories</div>`;
+      App.repositoriesField.empty().html(htmlrep);
     });
   });
 };
-$(document).ready(function() {
+$(document).ready(function () {
   App.userProfile = $('#user-profile');
   App.searchInput = $('#search-input');
   App.searchForm = $('#search-form');
