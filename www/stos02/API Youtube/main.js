cb50bcdd0ff6a64e5027e58e69c189a13c7fe578
Simona Stokingerova <stokingerovas@seznam.cz>
2018-12-16 22:23:31 +0100

    2. sem.prace

---
+++ www/stos02/API Youtube/main.js
@@ -0,0 +1,8 @@
+var appkey = "AIzaSyDFWvjr0kfp5dWp89C6_894J24akHha8sE";
+
+$(document).ready(function() {
+  $("#form").submit(function(e) {
+    e.preventDefault();
+    $("#results").html("");
+
+  
