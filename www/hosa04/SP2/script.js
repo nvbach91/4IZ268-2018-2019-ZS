a587f845e4aa7ce5be7d897d20755fe5042f36e6
adamhospodka <hospodka.adam@gmail.com>
2018-12-16 20:13:46 +0100

    semestral_improved

---
+++ www/hosa04/SP2/script.js
@@ -0,0 +1,69 @@
+var abeceda = "abcdefghijklmnopqrstuvwxyz";
+var pole = [ [0], [1], [2], [3], [4], [5], [6],[7],[8],[9],[10],[11],[12],[13],[14],[15],[16],[17],[18],[19],[20],[21],[22],[23],[24],[25] ]
+
+var words = ['diamant','auto','lahev'];
+var pom = new Array;
+var chosenWord = getRandomWord();
+
+var tabulka = new Array
+
+function createTajenka(secret) {
+    var result = "";
+    for (var i = 0; i < secret.length; i++) {
+        result += pom[i];
+    }
+    $('#secret').text(result);
+    $('#tip').val("");
+}
+
+function fillTajenka() {
+    for (var i = 0; i < chosenWord.length; i++) {
+        pom[i] = "-";
+    }
+    createTajenka(pom);
+}
+
+function getRandomWord(){
+    var wordIndex =  Math.floor(Math.random() * words.length);
+    console.log(words[wordIndex]);
+    return words[wordIndex];
+}
+
+function guessWord(p) {
+    $('#uhadol').text("");
+    if (chosenWord.match(p)) { //jake pismeno se nachazi v tejence
+        var x = abeceda.indexOf(p);
+        for (var i = 0; i < pole.length; i++) {
+            for (var j = 0; j < pole[i].length; j++) {
+                if (pole[i][j] == x) {
+                    var xy = pole[i];
+
+                }
+            }
+        }
+        for (var i = 0; i < xy.length; i++) {
+            for (var j = 0; j < chosenWord.length; j++){
+                if (chosenWord[j] == abeceda[xy[i]]) {
+                    pom[j] = abeceda[xy[i]];
+                }
+                else {
+                    if ([0][j] == p)
+                        pom[j] = p;
+                }
+            }
+        }
+        createTajenka(pom);
+    }
+    else {
+        $('#uhadol').text("Wrong");
+        $('#tip').val("");
+    }
+}
+
+$(function() {
+    $('#tip').focus();
+    fillTajenka();
+    $('#tip').change(function() {
+        guessWord($(this).val());
+    });
+});
