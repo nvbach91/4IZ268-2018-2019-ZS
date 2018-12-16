e4b14657b0a13e8be305d8933732f8dac1dd454a
Tomas Kaspar <tkaspar005@cee.ema.ad.pwcinternal.com>
2018-12-16 20:10:10 +0100

    Cteni a zapisovani z google sheets funkcni

--- www/kast00/semestralka/js/script.js
+++ www/kast00/semestralka/js/script.js
@@ -44,20 +44,176 @@ document.getElementById('btn3').onclick = function () { openRestaurant(event, 'r
 
 
 function openfloor(evt, floorName) {
-    var i, x, tabnavs;
+    var i, x, tabnavs2;
     x = document.getElementsByClassName("floor");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    tabnavs = document.getElementsByClassName("tabNav");
+    tabnavs2 = document.getElementsByClassName("tabNav2");
     for (i = 0; i < x.length; i++) {
-        tabnavs[i].className = tabnavs[i].className.replace("activeTab", "");
+        tabnavs2[i].className = tabnavs2[i].className.replace("activeTab", "");
     }
     document.getElementById(floorName).style.display = "block";
     evt.currentTarget.className += " activeTab";
 }
 
 
-document.getElementById('btn3').onclick = function () { openfloor(event, 'floor1'); }
-document.getElementById('btn4').onclick = function () { openfloor(event, 'floor2'); }
-document.getElementById('btn5').onclick = function () { openfloor(event, 'floor3'); }
+document.getElementById('btn4').onclick = function () { openfloor(event, 'floor1'); }
+document.getElementById('btn5').onclick = function () { openfloor(event, 'floor2'); }
+document.getElementById('btn6').onclick = function () { openfloor(event, 'floor3'); }
+
+
+
+
+function makeApiCall(action="read") {
+
+    var ssID = "1gjr8_KlM907EM06O_fOS92Qs9ns3lOwIVDjIXv0tNLc";
+    var rng = "List 1";
+
+    if (action == "write") {
+       
+        vals = new Array(4);
+        vals = [1, "automat 8", "11,11", "credit card accepted"]
+
+
+        var params = {
+            // The ID of the spreadsheet to update.
+            spreadsheetId: ssID,  // TODO: Update placeholder value.
+    
+            // The A1 notation of a range to search for a logical table of data.
+            // Values will be appended after the last row of the table.
+            range: rng,  // TODO: Update placeholder value.
+    
+            // How the input data should be interpreted.
+            valueInputOption: 'RAW',  // TODO: Update placeholder value.
+    
+            // How the input data should be inserted.
+            insertDataOption: 'INSERT_ROWS',  // TODO: Update placeholder value.
+          };
+    
+          var valueRangeBody = {
+            "values": [
+              [
+                1
+              ],
+              [
+                "automat 9"
+              ],
+              [
+                "11,11"
+              ],
+              [
+                "credit card accepted"
+              ]
+            ],
+            "majorDimension": "COLUMNS"
+          };
+    
+          var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
+          request.then(function(response) {
+            // TODO: Change code below to process the `response` object:
+            console.log(response.result);
+          }, function(reason) {
+            console.error('error: ' + reason.result.error.message);
+          });
+        
+    } else {
+
+        var params = {
+            // The ID of the spreadsheet to retrieve data from.
+            spreadsheetId: ssID,  // TODO: Update placeholder value.
+    
+            // The A1 notation of the values to retrieve.
+            range: rng,  // TODO: Update placeholder value.
+    
+            // How values should be represented in the output.
+            // The default render option is ValueRenderOption.FORMATTED_VALUE.
+            // valueRenderOption: '',  // TODO: Update placeholder value.
+    
+            // How dates, times, and durations should be represented in the output.
+            // This is ignored if value_render_option is
+            // FORMATTED_VALUE.
+            // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+            // dateTimeRenderOption: '',  // TODO: Update placeholder value.
+        };
+    
+        var request = gapi.client.sheets.spreadsheets.values.get(params);
+        request.then(function (response) {
+            // TODO: Change code below to process the `response` object:
+            console.log(response.result);
+            downloadData(response.result);
+        }, function (reason) {
+            console.error('error: ' + reason.result.error.message);
+        });
+    }
+    
+}
+
+function initClient() {
+    var API_KEY = 'AIzaSyAAQ6D3_bh2NZnrhzeLzP0ItURcjsAe13k';  // TODO: Update placeholder with desired API key.
+
+    var CLIENT_ID = '572059939133-5a7euvaedfvu0fi82gt918dn0apjrj57.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
+
+    // TODO: Authorize using one of the following scopes:
+    //   'https://www.googleapis.com/auth/drive'
+    //   'https://www.googleapis.com/auth/drive.file'
+    //   'https://www.googleapis.com/auth/drive.readonly'
+    //  'https://www.googleapis.com/auth/spreadsheets'
+    //  'https://www.googleapis.com/auth/spreadsheets.readonly'
+    var SCOPE = 'https://www.googleapis.com/auth/spreadsheets'
+        ;
+
+    gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function () {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+}
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function updateSignInStatus(isSignedIn) {
+    if (isSignedIn) {
+        makeApiCall();
+    }
+}
+
+function handleSignInClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignOutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+function downloadData(result) {
+    for (var row = 0; row < 8; row++) {
+        var x = document.createElement("IMG");
+        x.setAttribute("src", "img/location.png");
+      x.setAttribute("class", "pin");
+
+        for (var col = 0; col < 4; col++) {
+            if (col = 2) {
+                var res = result.values[row][col].split(","); 
+                var style = "top: " + res[0] + "%; left: " + res[1] + "%;";
+                x.setAttribute("style", style)
+              console.log(style);
+              break;
+        } 
+
+    }
+
+    document.getElementById("floor1mapa").appendChild(x);
+}
+}
+
+function refresh() {
+    document.getElementById("floor1mapa").innerHTML = "";
+    makeApiCall();
+}
