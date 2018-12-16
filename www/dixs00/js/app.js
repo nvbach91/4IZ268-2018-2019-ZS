4720e16ce359ac5985d45ee59050fb54a4b1a327
Sebastian Dix <mail@lektorpraha.cz>
2018-12-16 23:44:34 +0100

    Stylovani button a dropdown menu

--- www/dixs00/js/app.js
+++ www/dixs00/js/app.js
@@ -8,39 +8,50 @@ $(document).ready(function () {
         .then(data => data.json())
         .then(data => {
             data = JSON.parse(data);
-            var topicsList = $('#topics');
+            var topicDropdown = $('#topicDropdown');
 
             $.each(data, function (i) {
-                var li = $(`<li>${data[i]}</li>`)
+                var option = $(`<option>${ data[i] }</option>`)
                     .addClass("topic-item")
-                    .appendTo(topicsList);
+                    .appendTo(topicDropdown);
             });
-            console.log(document.querySelector(".topic-item").innerHTML);
 
             /*Teprve na kliknuti se zavola dalsi pozadavek */
-            $(".topic-item").on("click", function () {
-                var topicName = this.innerHTML;
+            $("#practiceButton").on("click", function () {
+                console.log("button clicked")
+                var e = document.getElementById("topicDropdown");
+                var selectedOptionIndex = e.options.selectedIndex;
+                var selectedOptionText = e.options[selectedOptionIndex].text;
+                var topicName = selectedOptionText;
                 var vocab = fetch('https://pure-chamber-44082.herokuapp.com/api/topics/' + topicName + '/vocab', {
                     mode: 'cors',
                     headers: {
                         'Access-Control-Allow-Origin': '*'
                     }
                 })
-                .then(data => data.json())
+                    .then(data => data.json())
                     .then(
                         data => {
-
                             data = JSON.parse(data);
                             vocabList = $("#vocab");
-                            
-                         for (let i = 0; i < data.length; i++) {
-                             const element = data[i];
-                             var li = $(`<li>${element}</li>`)
-                             .addClass("vocab-item")
-                             .appendTo(vocabList);
-                             
 
-                         }
+                            for (let i = 0; i < data.length; i++) {
+                                const pair = data[i];
+                                let word = pair[Object.keys(pair)[0]];
+                                var li = $(`<li>${ word }</li>`)
+                                    .addClass("vocab-item")
+                                    .appendTo(vocabList);
+
+                                /* Addneš event listener k jednotlivým kartičkám, které je otočí. 
+                                Do nějakýho session storage si uložíš aktuálně procvičovanou kartičku, stačí index
+                                a pak z vocab listu vybíráš nth child a dáš display: něco
+                                a zbytek bude display: none
+                                
+                                navíc musíš z toho "append to" udělat spíš toggle funkci, že se to vypíše a pak zase schová
+                                
+                                musíš taky nastylovat ty kartičky
+                                */
+                            }
                         }
 
                     )
