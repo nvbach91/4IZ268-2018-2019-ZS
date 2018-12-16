5e8f18445c7d4be30a140b2a5f16a3ba5e33b0db
Sebastian Dix <mail@lektorpraha.cz>
2018-12-16 08:29:08 +0100

    Data retrieval works, now for the user interaction and styling

--- www/dixs00/js/app.js
+++ www/dixs00/js/app.js
@@ -1,45 +1,77 @@
-$(document).ready(function(){
-    var topics = fetch('https://pure-chamber-44082.herokuapp.com/api/topics',{  mode: 'cors',
-    headers: {
-      'Access-Control-Allow-Origin':'*'
-    }})
-        .then(data => data.json())  
-        .then(data => {
-            data = JSON.parse(data);
-        var topicsEl = $('#topics');
-    
-        $.each(data, function(i){
-            var li = $(`<li>${data[i]}</li>`)
-                .addClass("topic-item")
-                .appendTo(topicsEl);
-        });
-        console.log(document.querySelector(".topic-item").innerHTML);
-    })
-
-    $(".topic-item").on("click",function() {
-        console.log(document.querySelector(".topic-item").innerHTML);
-        console.log($(this).innerHTML);
-        // Vytahnout na co jsem kliknul
-        // Zeditujes URL tak ze na konec pridas topic na ktery jsi klikl napr https://pure-chamber-44082.herokuapp.com/api/topics/nature/vocab
-        var vocab = fetch('https://pure-chamber-44082.herokuapp.com/api/topics/work/vocab',{  mode: 'cors',
+$(document).ready(function () {
+    var topics = fetch('https://pure-chamber-44082.herokuapp.com/api/topics', {
+        mode: 'cors',
         headers: {
-          'Access-Control-Allow-Origin':'*'
-        }})
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
         .then(data => data.json())
         .then(data => {
             data = JSON.parse(data);
-            console.log(data)
+            var topicsList = $('#topics');
 
-        var vocabEl = $('#vocab');
-    
-        $.each(data, function(i){
-            var li = $(`<li>${data[i]}</li>`)
-                .addClass('vocab-item')
-                .appendTo(vocabEl);
-        });
-    })
-    })
+            $.each(data, function (i) {
+                var li = $(`<li>${data[i]}</li>`)
+                    .addClass("topic-item")
+                    .appendTo(topicsList);
+            });
+            console.log(document.querySelector(".topic-item").innerHTML);
 
-    
+            /*Teprve na kliknuti se zavola dalsi pozadavek */
+            $(".topic-item").on("click", function () {
+                var topicName = this.innerHTML;
+                var vocab = fetch('https://pure-chamber-44082.herokuapp.com/api/topics/' + topicName + '/vocab', {
+                    mode: 'cors',
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                })
+                .then(data => data.json())
+                    .then(
+                        data => {
+
+                            data = JSON.parse(data);
+                            vocabList = $("#vocab");
+                            
+                         for (let i = 0; i < data.length; i++) {
+                             const element = data[i];
+                             var li = $(`<li>${element}</li>`)
+                             .addClass("vocab-item")
+                             .appendTo(vocabList);
+                             
+
+                         }
+                        }
+
+                    )
+            })
+
+        })
 })
+//     $(".topic-item").on("click",function() {
+//         console.log(document.querySelector(".topic-item").innerHTML);
+//         console.log($(this).innerHTML);
+
+//         var vocab = fetch('https://pure-chamber-44082.herokuapp.com/api/topics/work/vocab',{  mode: 'cors',
+//         headers: {
+//           'Access-Control-Allow-Origin':'*'
+//         }})
+//         .then(data => data.json())
+//         .then(data => {
+//             data = JSON.parse(data);
+//             console.log(data)
+
+//         var vocabEl = $('#vocab');
+
+//         $.each(data, function(i){
+//             var li = $(`<li>${data[i]}</li>`)
+//                 .addClass('vocab-item')
+//                 .appendTo(vocabEl);
+//         });
+//     })
+//     })
+//     )
+
+
+// })
 
