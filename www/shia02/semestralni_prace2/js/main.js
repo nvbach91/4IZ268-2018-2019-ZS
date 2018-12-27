2754ce8391454724491985eb4b9dacfc6a23c8e0
Asya96 <shikhlyarova@seznam.cz>
2018-12-27 22:58:13 +0100

    pridani vyberu poctu radku, delky pro vyhru, kdo je na tahu

--- www/shia02/semestralni_prace2/js/main.js
+++ www/shia02/semestralni_prace2/js/main.js
@@ -1,4 +1,6 @@
 var gameField = $('#game-field');
+var playerOnTurnDiv = $('#playerOnTurn');
+var turnDiv = $('#turnDiv');
 var gameOver = false;
 var playerOnTurn = 1;
 
@@ -20,13 +22,33 @@ function createArray(length) {
     for (var i = 0; i < length; i++) {
         arr[i] = new Array(length);
     }
-
     return arr;
 }
 
-// create game field
+// input lenght of win
+var winLength = function () {
+    var winLength = parseInt($('#winLength').val());
+
+    if (Number.isNaN(winLength)) {
+        return null;
+    }
+
+    return winLength;
+}
+// input size of row
+var sizeOfRow = function () {
+    var sizeOfRow = parseInt($('#sizeOfRow').val());
+
+    if (Number.isNaN(sizeOfRow)) {
+        return null;
+    }
+
+    return sizeOfRow;
+};
+
 var size = 5;
 var winSize = 3;
+
 var gameArr = createArray(size);
 var freeCellsCount = size * size;
 
@@ -143,51 +165,108 @@ var isWinner = function (gameArr, player) {
 
 };
 
-for (var i = 0; i < size; i++) {
-    var row = createRow();
-    for (var j = 0; j < size; j++) {
-        var cell = createCell();
-
-        // function invoked on user click
-        cell.addEventListener('click', function () {
-            if (gameOver) {
-                return;
-            }
-            
-            var columnIndex = $(this).index();
-            var rowIndex = $(this.parentNode).index();
-
-            // error checking
-            if (gameArr[rowIndex][columnIndex] != null) {
-                return;
-            }
-
-            var previousPlayerOnTurn = playerOnTurn;
-            gameArr[rowIndex][columnIndex] = playerOnTurn;
-            if (playerOnTurn === 1) {
-                this.innerText = 'X';
-                playerOnTurn = 2;
-            } else {
-                this.innerText = 'O';
-                playerOnTurn = 1;
-            }
-            freeCellsCount--;
-
-            if (isWinner(gameArr, previousPlayerOnTurn)) { 
-                alert('Hráč ' + playerOnTurn + ' vyhrál');
-                // TODO: zamkni hrací plochu
-                gameOver = true;
-            }
-            else if (freeCellsCount === 0) {
-                alert('Pole je plné, hra skoncila remizou.');
-                gameOver = true;
-            }
-
-
-        });
-
-        row.append(cell);
+var playerSymbol = function(player) {
+    if (player === 1) {
+        return "X";
     }
-    gameField.append(row);
+    if (player === 2) {
+        return "O";
+    }
+    
+    return null;
+}
+
+var updatePlayerOnTurnDiv = function (playerOnTurn) {
+    var symbol = playerSymbol(playerOnTurn);
+    playerOnTurnDiv.text(symbol);
 };
+
+// create game field
+var startGame = function () {
+    var controlSize = sizeOfRow();
+    var controlWinSize = winLength();
+
+    if (controlSize === null || controlWinSize === null) {
+        alert("Je potreba vyplnit vsechna pole");
+        return;
+    }
+
+    if (controlWinSize > controlSize) {
+        alert("PROBLEM");
+        return;
+    }
+
+    if (controlSize < 3 || controlSize > 15) {
+        alert("PROBLEM");
+        return;
+    }
+    size = controlSize;
+
+    if (controlWinSize < 3 || controlWinSize > 5) {
+        alert("PROBLEM");
+        return;
+    }
+    winSize = controlWinSize;
+
+    gameArr = createArray(size);
+    freeCellsCount = size * size;
+    gameOver = false;
+    playerOnTurn = 1;
+    gameField.empty();
+    turnDiv.show();
+
+    for (var i = 0; i < size; i++) {
+        var row = createRow();
+        for (var j = 0; j < size; j++) {
+            var cell = createCell();
+
+            // function invoked on user click
+            cell.addEventListener('click', function () {
+                if (gameOver) {
+                    return;
+                }
+
+                var columnIndex = $(this).index();
+                var rowIndex = $(this.parentNode).index();
+
+                // error checking
+                if (gameArr[rowIndex][columnIndex] != null) {
+                    return;
+                }
+
+                var previousPlayerOnTurn = playerOnTurn;
+                gameArr[rowIndex][columnIndex] = playerOnTurn;
+                this.innerText = playerSymbol(playerOnTurn);
+                if (playerOnTurn === 1) {
+                    playerOnTurn = 2;
+                } else {
+                    playerOnTurn = 1;
+                }
+                freeCellsCount--;
+                updatePlayerOnTurnDiv(playerOnTurn);
+
+                if (isWinner(gameArr, previousPlayerOnTurn)) {
+                    alert('Hráč ' + playerSymbol(previousPlayerOnTurn) + ' vyhrál');
+                    // TODO: zamkni hrací plochu
+                    gameOver = true;
+                    turnDiv.hide();
+                }
+                else if (freeCellsCount === 0) {
+                    alert('Pole je plné, hra skoncila remizou.');
+                    gameOver = true;
+                    turnDiv.hide();
+                }
+
+
+            });
+
+            row.append(cell);
+        }
+        gameField.append(row);
+    };
+
+}
+
+//startGame();
+
 
