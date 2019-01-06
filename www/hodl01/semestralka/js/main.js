6f1f6838ee24b2a601ddf18227a7ed4a2fd33fe0
Lukáš Hodboď <lukashodbod@Lukas-MacBook-Pro.local>
2019-01-06 18:46:38 +0100

    nacitani z dalsiho profilu

--- www/hodl01/semestralka/js/main.js
+++ www/hodl01/semestralka/js/main.js
@@ -1,21 +1,40 @@
 $(function () {
     //Instagram API
-    var token = '1404870044.fa0d083.290093ca2a4d4a7f90e28bf31ec3570d',
-        num_photos = 18, // max 20
-        container = document.getElementById('instafeed'),
-        scrElement = document.createElement('script');
+    var ptoken = '1404870044.fa0d083.290093ca2a4d4a7f90e28bf31ec3570d',
+        mtoken = '6320184047.209a707.58363885aaa94bcdb68332026275b3f1',
+        container = document.getElementById('instafeed');
 
-    window.mishaProcessResult = function (data) {
-        for (x in data.data) {
-            container.innerHTML += '<div class="photoBox"><img onload="fillByImage(this)" src="' + data.data[x].images.low_resolution.url + '"></div>';
+        window.mishaProcessResult = function (data) {
+            for (x in data.data) {
+                container.innerHTML += '<div class="photoBox"><img onload="fillByImage(this)" src="' + data.data[x].images.low_resolution.url + '"></div>';
+            }
         }
-    }
+    apiScriptCreate(ptoken);
 
-    scrElement.setAttribute('src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult');
-    document.body.appendChild(scrElement);
 
+    $('button').click(function () {
+        $( '.apiScript' ).remove();
+        $( '.photoBox').remove();
+        if (this.innerHTML=='@makacenkoteam') {
+            apiScriptCreate(mtoken);
+            this.innerHTML='@lukashodbod';
+            $('h2').innerHTML='Instagram @makacenkoteam';
+        } else {
+            apiScriptCreate(ptoken);
+            this.innerHTML='@makacenkoteam';
+            $('h2').innerHTML='Instagram @lukashodbod';
+        }
+    });
+   
 
 });
+function apiScriptCreate(token) {
+    var num_photos = 18;
+    var scrElement = document.createElement('script');
+    scrElement.classList = 'apiScript';
+    scrElement.setAttribute('src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult');
+    document.body.appendChild(scrElement);
+}
 function fillByImage(t) {
     var p = t.parentElement;
     var pRatio = 1, tRatio = t.naturalWidth / t.naturalHeight;
