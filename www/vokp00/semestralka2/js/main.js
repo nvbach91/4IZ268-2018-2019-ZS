f7057401631b8224372abb6deb0d20a7881b297b
Petra Vokálová <Blondie@petra-macbook-air.home>
2019-01-06 12:05:33 +0100

    jquery vytvoření + pokusy přes fetch, getjson,wait/async v průběhu prázdnin

--- www/vokp00/semestralka2/js/main.js
+++ www/vokp00/semestralka2/js/main.js
@@ -5,7 +5,8 @@ var city = "";
 
 /* Získání délky, šířky a města uživatele */
 
-fetch("http://ip-api.com/json/?fields=lat,lon,city").then(function (response) {
+/*
+fetch("http://extreme-ip-lookup.com/json/").then(function (response) {
     return response.json();
 }).then(function (response) {
     latitude = response.lat;
@@ -13,40 +14,54 @@ fetch("http://ip-api.com/json/?fields=lat,lon,city").then(function (response) {
     city = response.city;
     console.log(latitude, longitude, city);
 });
+*/
+
+
 
 /* nastavení mapy */
-var center = SMap.Coords.fromWGS84(latitude, longitude);
-var map = new SMap(JAK.gel("map"), center, 9);
-var layerTwo = new SMap.Layer.Marker();
-map.addLayer(layerTwo);
-layerTwo.enable();
+for (; latitude === 0;) {
 
-/* Aby mapa reagovala na změnu velikosti průhledu */
-map.addControl(new SMap.Control.Sync());
+    $.getJSON("http://extreme-ip-lookup.com/json/", function (location) {
+        var locationInfo = [];
+        locationInfo = location;
+        latitude = locationInfo.lat;
+        longitude = locationInfo.lon;
+        city = locationInfo.city;
+        console.log(latitude, longitude, city);
 
-/* Turistický podklad*/
-map.addDefaultLayer(SMap.DEF_TURIST).enable();
+    });
 
-/* Ovládání myší*/
-var mouse = new SMap.Control.Mouse(SMap.MOUSE_PAN | SMap.MOUSE_WHEEL | SMap.MOUSE_ZOOM);
-map.addControl(mouse);
+    console.log(latitude, longitude, city);
 
-/* Ovládací prvky */
-map.addDefaultControls();
+    var center = SMap.Coords.fromWGS84(longitude, latitude);
+    var map = new SMap(JAK.gel("map"), center, 9);
+    /* Aby mapa reagovala na změnu velikosti průhledu */
+    map.addControl(new SMap.Control.Sync());
+    /* Turistický podklad*/
+    map.addDefaultLayer(SMap.DEF_TURIST).enable();
+    /* Ovládání myší*/
+    var mouse = new SMap.Control.Mouse(SMap.MOUSE_PAN | SMap.MOUSE_WHEEL | SMap.MOUSE_ZOOM);
+    map.addControl(mouse);
+    /* Ovládací prvky */
+    map.addDefaultControls();
 
-/* Získání souboru do mapy z gpx se vyrobí xml */
-var xhr = new JAK.Request(JAK.Request.XML);
-xhr.setCallback(window, "response");
-xhr.send("gpx/hrady.gpx");
+    var layerTwo = new SMap.Layer.Marker();
+    map.addLayer(layerTwo);
+    layerTwo.enable();
 
-/* přidání vrstvy pro zobrazení bodů na mapě */
-var response = function (xmlDoc) {
-    var gpx = new SMap.Layer.GPX(xmlDoc, null, { maxPoints: 100 });
-    map.addLayer(gpx);
-    gpx.enable();
-    /*gpx.fit();*/
+    /* Získání souboru do mapy */
+    var xhr = new JAK.Request(JAK.Request.XML);
+    xhr.setCallback(window, "response");
+    xhr.send("gpx/hrady.gpx");
+
+    /* přidání vrstvy pro zobrazení bodů na mapě */
+    var response = function (xmlDoc) {
+        var gpx = new SMap.Layer.GPX(xmlDoc, null, { maxPoints: 100 });
+        map.addLayer(gpx);
+        gpx.enable();
+        /*gpx.fit();*/
+    };
 }
-
 var latitudeCastle = 0;
 var longitudeCastle = 0;
 var nameCastle = "";
@@ -61,8 +76,8 @@ $.getJSON("gpx/hrady.json", function (data) {
     for (var i = 0; i < JSONItems.features.length; i++) {
 
 
-        latitudeCastle = JSONItems.features[i].geometry.coordinates[0];
-        longitudeCastle = JSONItems.features[i].geometry.coordinates[1];
+        latitudeCastle = JSONItems.features[i].geometry.coordinates[1];
+        longitudeCastle = JSONItems.features[i].geometry.coordinates[0];
         nameCastle = JSONItems.features[i].properties.name;
         descCastle = JSONItems.features[i].properties.desc;
         link = JSONItems.features[i].properties.links[0].href;
@@ -81,8 +96,6 @@ $.getJSON("gpx/hrady.json", function (data) {
         dist = Math.acos(dist);
         dist = dist * 180 / Math.PI;
         dist = dist * 60 * 1.1515;
-        if (unit == "K") { dist = dist * 1.609344 }
-        if (unit == "N") { dist = dist * 0.8684 }
 
         /* uložení vzdáleností do pole */
         array.push(dist);
@@ -96,28 +109,61 @@ $.getJSON("gpx/hrady.json", function (data) {
     }
 });
 
-/* Vaše pozice dle IP adresy, informace o nejbližším hradu a jeho webu */
+/* Vaše pozice dle IP adresy, informace o nejbližším hradu, trase a jeho webu */
 var buttonclicked;
 
 $("#coors").click(function () {
     if (buttonclicked != true) {
         buttonclicked = true;
-        var para = document.createElement("P");
-        var t = document.createTextNode("You are somewhere near the coordinates " + latitude + ", " + longitude + " which means you are in " + city +
+
+        $('coordinates').append("<p id='p'></p>");
+        $('p').append("<br>You are somewhere near the coordinates " + latitude + ", " + longitude + " which means you are in " + city +
             ". The closest castle you can visit is: " + nameCastle + ". " + " Basic info: " + descCastle +
             " You can get more info here: ");
-        para.appendChild(t);
-        var a = document.createElement('a');
-        var linkText = document.createTextNode(link);
-        a.appendChild(linkText);
-        a.href = link;
-        para.appendChild(a);
+        var thelink = $('<a>', {
+            text: link,
+            title: 'Castle page',
+            href: link
+        }).appendTo('p');
 
-        document.getElementById("coordinates").appendChild(para);
+        /*
+                var para = document.createElement("P");
+                var t = document.createTextNode("You are somewhere near the coordinates " + latitude + ", " + longitude + " which means you are in " + city +
+                    ". The closest castle you can visit is: " + nameCastle + ". " + " Basic info: " + descCastle +
+                    " You can get more info here: ");
+                para.appendChild(t);
+                var a = document.createElement('a');
+                var linkText = document.createTextNode(link);
+                a.appendChild(linkText);
+                a.href = link;
+                para.appendChild(a);
+        
+                document.getElementById("coordinates").appendChild(para);
+        */
+
+        var nalezeno = function (route) {
+            var vrstva = new SMap.Layer.Geometry();
+            map.addLayer(vrstva).enable();
+
+            var coords = route.getResults().geometry;
+            var cz = map.computeCenterZoom(coords);
+            map.setCenterZoom(cz[0], cz[1]);
+            var g = new SMap.Geometry(SMap.GEOMETRY_POLYLINE, null, coords);
+            vrstva.addGeometry(g);
+        }
+
+        var coords = [
+            SMap.Coords.fromWGS84(longitude, latitude),
+            SMap.Coords.fromWGS84(longitudeCastle, latitudeCastle)
+        ];
+        var route = new SMap.Route(coords, nalezeno);
+
 
     } else {
-        alert("Hey, you already know your location and the closest castle.");
+        alert("Hey, you already know your location and the way to the closest castle.");
     }
+
+
 });
 
 
