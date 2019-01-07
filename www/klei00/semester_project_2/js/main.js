8a84237f89cdce7aa65abd7afece884c197afa44
Iveta Kleníková <iveta.klenikova@seznam.cz>
2019-01-08 00:03:44 +0100

    Rating and localStorage added

--- www/klei00/semester_project_2/js/main.js
+++ www/klei00/semester_project_2/js/main.js
@@ -53,7 +53,6 @@ form.submit(function (e) {
                 resultsField = $('<div>').attr('id', 'results-field');
                 formBody.append(resultsField);
             }
-
             if (response.totalItems === 0) {
                 var result = $('<div>').addClass('result-row').text('Nebyla nalezena žádná kniha.');
                 resultsField.append(result);
@@ -111,7 +110,7 @@ var addToResults = function (item) {
         if (bookExists(item.id)) {
             alert('Tuto knihu již máš ve své knihovně uloženou.');
         } else {
-            addToLibrary(item, author, imageUrl);
+            addToLibrary(item, author, imageUrl, 0);
             addButton.addClass('add-existing');
         }
     });
@@ -119,7 +118,9 @@ var addToResults = function (item) {
     resultsField.append(resultRow);
 }
 /*--------------------- add a book to my library -----------------------------------------*/
-var addToLibrary = function (book, author, url) {
+var addToLibrary = function (book, author, url, rating) {
+    localStorage.setItem(book.id, JSON.stringify([JSON.stringify(book), 0]));
+
     var imageCell = $('<div>').addClass('table-cell').append($('<img>').addClass('image').attr('src', url));
     var nameCell = $('<div>').addClass('table-cell').text(book.volumeInfo.title);
     var authorCell = $('<div>').addClass('table-cell').text(author);
@@ -143,9 +144,17 @@ var addToLibrary = function (book, author, url) {
         }
     }
     var categoryCell = $('<div>').addClass('table-cell').text(category);
+
+    var star1 = $('<span>').addClass('star').text('*');
+    var star2 = $('<span>').addClass('star').text('*');
+    var star3 = $('<span>').addClass('star').text('*');
+    var star4 = $('<span>').addClass('star').text('*');
+    var star5 = $('<span>').addClass('star').text('*');
+
+    var ratingCell = $('<div>').addClass('table-cell').addClass('rating').append(star1).append(star2).append(star3).append(star4).append(star5);
     var deleteCell = $('<div>').addClass('table-cell').addClass('delete').text('Odebrat');
     var idCell = $('<div>').addClass('table-cell').addClass('id').text(book.id);
-    var newRow = $('<div>').addClass('table-row').append(imageCell).append(nameCell).append(authorCell).append(categoryCell).append(yearCell).append(deleteCell).append(idCell);
+    var newRow = $('<div>').addClass('table-row').append(imageCell).append(nameCell).append(authorCell).append(categoryCell).append(yearCell).append(ratingCell).append(deleteCell).append(idCell);
     var tableHead = $('#table-head');
     if (tableHead.hasClass('closed')) {
         tableHead.removeClass('closed');
@@ -153,15 +162,173 @@ var addToLibrary = function (book, author, url) {
     $('#my-library').append(newRow);
 
     deleteCell.click(function () {
+        localStorage.removeItem(book.id);
         newRow.remove();
         if ($('.id').length === 1) {
             tableHead.addClass('closed');
         }
         form.submit();
     });
+
+    star1.click(function () {
+        if (!star1.hasClass('checked')) {
+            star1.addClass('checked');
+        } else {
+            if (star2.hasClass('checked')) {
+                star2.removeClass('checked');
+            }
+            if (star3.hasClass('checked')) {
+                star3.removeClass('checked');
+            }
+            if (star4.hasClass('checked')) {
+                star4.removeClass('checked');
+            }
+            if (star5.hasClass('checked')) {
+                star5.removeClass('checked');
+            }
+        }
+        var changedRating = JSON.parse(localStorage.getItem(book.id));
+        changedRating[1] = 1;
+        localStorage.removeItem(book.id);
+        localStorage.setItem(book.id, JSON.stringify(changedRating));
+        form.submit();
+    });
+    star2.click(function () {
+        if (!star1.hasClass('checked')) {
+            star1.addClass('checked');
+        }
+        if (!star2.hasClass('checked')) {
+            star2.addClass('checked');
+        } else {
+            if (star3.hasClass('checked')) {
+                star3.removeClass('checked');
+            }
+            if (star4.hasClass('checked')) {
+                star4.removeClass('checked');
+            }
+            if (star5.hasClass('checked')) {
+                star5.removeClass('checked');
+            }
+        }
+        var changedRating = JSON.parse(localStorage.getItem(book.id));
+        changedRating[1] = 2;
+        localStorage.removeItem(book.id);
+        localStorage.setItem(book.id, JSON.stringify(changedRating));
+        form.submit();
+    });
+    star3.click(function () {
+        if (!star1.hasClass('checked')) {
+            star1.addClass('checked');
+        }
+        if (!star2.hasClass('checked')) {
+            star2.addClass('checked');
+        }
+        if (!star3.hasClass('checked')) {
+            star3.addClass('checked');
+        } else {
+            if (star4.hasClass('checked')) {
+                star4.removeClass('checked');
+            }
+            if (star5.hasClass('checked')) {
+                star5.removeClass('checked');
+            }
+        }
+        var changedRating = JSON.parse(localStorage.getItem(book.id));
+        changedRating[1] = 3;
+        localStorage.removeItem(book.id);
+        localStorage.setItem(book.id, JSON.stringify(changedRating));
+        form.submit();
+    });
+    star4.click(function () {
+        if (!star1.hasClass('checked')) {
+            star1.addClass('checked');
+        }
+        if (!star2.hasClass('checked')) {
+            star2.addClass('checked');
+        }
+        if (!star3.hasClass('checked')) {
+            star3.addClass('checked');
+        }
+        if (!star4.hasClass('checked')) {
+            star4.addClass('checked');
+        } else {
+            if (star5.hasClass('checked')) {
+                star5.removeClass('checked');
+            }
+        }
+        var changedRating = JSON.parse(localStorage.getItem(book.id));
+        changedRating[1] = 4;
+        localStorage.removeItem(book.id);
+        localStorage.setItem(book.id, JSON.stringify(changedRating));
+        form.submit();
+    });
+    star5.click(function () {
+        if (!star1.hasClass('checked')) {
+            star1.addClass('checked');
+        }
+        if (!star2.hasClass('checked')) {
+            star2.addClass('checked');
+        }
+        if (!star3.hasClass('checked')) {
+            star3.addClass('checked');
+        }
+        if (!star4.hasClass('checked')) {
+            star4.addClass('checked');
+        }
+        if (!star5.hasClass('checked')) {
+            star5.addClass('checked');
+        }
+        var changedRating = JSON.parse(localStorage.getItem(book.id));
+        changedRating[1] = 5;
+        localStorage.removeItem(book.id);
+        localStorage.setItem(book.id, JSON.stringify(changedRating));
+        form.submit();
+    });
+    switch (rating) {
+        case 1: star1.trigger('click');
+            break;
+        case 2: star2.trigger('click');
+            break;
+        case 3: star3.trigger('click');
+            break;
+        case 4: star4.trigger('click');
+            break;
+        case 5: star5.trigger('click');
+    }
 }
 /* -------------- create loader --------------------------*/
 var createLoader = function () {
     var loader = $('<div>').addClass('loader').append($('<figure>').addClass('page'));
     $(document.body).append($('<div>').addClass('whole-page').append(loader));
+}
+
+/* ----------------- init page ----------------------- */
+if (localStorage.length !== 0) {
+    for (var i = 0; i < localStorage.length; i++) {
+        var content = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        var book = JSON.parse(content[0]);
+        var rating = content[1];
+
+        var author = '';
+        if (book.volumeInfo.authors === undefined) {
+            author = 'Neznámý autor';
+        } else {
+            var authors = book.volumeInfo.authors;
+            if (authors.length === 1) {
+                author = authors[0];
+            } else {
+                author = authors[0];
+                for (var j = 0; j < authors.length; j++) {
+                    author += ', ' + authors[j];
+                }
+            }
+        }
+        var imageUrl;
+        if (book.volumeInfo.imageLinks === undefined) {
+            imageUrl = 'https://books.google.cz/googlebooks/images/no_cover_thumb.gif';
+        } else {
+            imageUrl = book.volumeInfo.imageLinks.smallThumbnail;
+        }
+        addToLibrary(book, author, imageUrl, rating);
+    }
 }
