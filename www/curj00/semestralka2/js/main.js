045250bf857b6409fea5de4d479f27ed5dd003f0
jcurik <jancurik1@gmail.com>
2019-01-13 22:21:27 +0100

    Další obsah a funkce - trasování, získávání dat ze souboru pomocí AJAX, vizitky dealerů s popisem, změna zápisu dat, atd.

--- www/curj00/semestralka2/js/main.js
+++ www/curj00/semestralka2/js/main.js
@@ -1,5 +1,6 @@
 // var obrazek = "https://api.mapy.cz/img/api/marker/drop-red.png";
 var obrazek = "img/drop-vw.png";
+var obrazek_start = "img/drop-vw2.png";
 var mapa = new SMap(JAK.gel("mapa"));
 mapa.addControl(new SMap.Control.Sync()); /* Aby mapa reagovala na změnu velikosti průhledu */
 mapa.addDefaultLayer(SMap.DEF_BASE).enable(); /* Turistický podklad */
@@ -12,7 +13,7 @@ mapa.addDefaultLayer(SMap.DEF_HISTORIC);
 mapa.addDefaultLayer(SMap.DEF_OPHOTO0203);
 mapa.addDefaultLayer(SMap.DEF_OPHOTO0406);
 
-// Možnost typ změnit mapu
+// Možnost změnit typ mapy
 var layerSwitch = new SMap.Control.Layer();
 layerSwitch.addDefaultLayer(SMap.DEF_BASE);
 layerSwitch.addDefaultLayer(SMap.DEF_OPHOTO);
@@ -22,65 +23,207 @@ layerSwitch.addDefaultLayer(SMap.DEF_OPHOTO0203);
 layerSwitch.addDefaultLayer(SMap.DEF_HISTORIC);
 mapa.addControl(layerSwitch, { left: "10px", top: "7px" });
 
-var data = {
-    "NH Car": "50°5'1.732\"N, 14°23'3.251\"E",
-    "Porsche Praha Smíchov": "50°4'10.489\"N, 14°22'41.630\"E",
-    "Porsche Praha Prosek": "50°7'55.227\"N, 14°29'30.433\"E",
-    "Auto Jarov": "50°5'25.698\"N, 14°29'56.907\"E",
-    "Auto Podbabská Praha": "50°6'52.443\"N, 14°23'30.614\"E",
-    "Autosalon Klokočka Centrum": "50°4'35.496\"N, 14°17'56.176\"E",
-    "TUkas": "50°0'7.182\"N, 14°24'45.595\"E",
-    "AUTO JAROV KUNRATICE": "50°0'38.903\"N, 14°28'36.962\"E",
-    "Louda Auto Praha": "50°6'29.972\"N, 14°32'54.237\"E",
-    "Auto I.S.R.": "50°0'18.520\"N, 14°34'10.862\"E"
-};
+var data = [{
+    name: "NH Car",
+    id: 1,
+    coords: "50°5'1.732\"N, 14°23'3.251\"E"
+}, {
+    name: "Porsche Praha Smíchov",
+    id: 2,
+    coords: "50°4'10.489\"N, 14°22'41.630\"E"
+}, {
+    name: "Porsche Praha Prosek",
+    id: 3,
+    coords: "50°7'55.227\"N, 14°29'30.433\"E"
+}, {
+    name: "Auto Jarov",
+    id: 4,
+    coords: "50°5'25.698\"N, 14°29'56.907\"E"
+}, {
+    name: "Auto Podbabská Praha",
+    id: 5,
+    coords: "50°6'52.443\"N, 14°23'30.614\"E"
+}, {
+    name: "Autosalon Klokočka Centrum",
+    id: 6,
+    coords: "50°4'35.496\"N, 14°17'56.176\"E"
+}, {
+    name: "TUkas",
+    id: 7,
+    coords: "50°0'7.182\"N, 14°24'45.595\"E"
+}, {
+    name: "AUTO JAROV KUNRATICE",
+    id: 8,
+    coords: "50°0'38.903\"N, 14°28'36.962\"E"
+}, {
+    name: "Louda Auto Praha",
+    id: 9,
+    coords: "50°6'29.972\"N, 14°32'54.237\"E"
+}, {
+    name: "Auto I.S.R.",
+    id: 10,
+    coords: "50°0'18.520\"N, 14°34'10.862\"E"
+}];
+
 var znacky = [];
 var souradnice = [];
+var startovniBod;
 
-for (var name in data) { /* Vyrobit značky */
-    var c = SMap.Coords.fromWGS84(data[name]); /* Souřadnice značky, z textového formátu souřadnic */
-
+data.forEach(function (marker) { /* Vyrobit markery */
+    var c = SMap.Coords.fromWGS84(marker.coords); /* Souřadnice značky, z textového formátu souřadnic */
     var options = {
         url: obrazek,
-        title: name,
+        title: marker.name,
         anchor: { left: 10, bottom: 1 }  /* Ukotvení značky za bod uprostřed dole */
     }
-
-    var znacka = new SMap.Marker(c, null, options);
+    // Důležité je přiřazení ID jednotlivým markerům - vlastní ID, jinak se generuje nahodne
+    var znacka = new SMap.Marker(c, marker.id, options);
     souradnice.push(c);
     znacky.push(znacka);
-}
-
+});
 
 /* NH Car ukotvíme za střed značky, přestože neznáme její velikost */
-var options = {
-    anchor: { left: 0.5, top: 0.5 }
-}
-znacky[1].decorate(SMap.Marker.Feature.RelativeAnchor, options);
+// var options = {
+//     anchor: { left: 0.5, top: 0.5 }
+// }
+// znacky[1].decorate(SMap.Marker.Feature.RelativeAnchor, options);
+
 
 var vrstva = new SMap.Layer.Marker();     /* Vrstva se značkami */
 mapa.addLayer(vrstva);                          /* Přidat ji do mapy */
 vrstva.enable();                         /* A povolit */
 for (var i = 0; i < znacky.length; i++) {
     vrstva.addMarker(znacky[i]);
+    var card = new SMap.Card();
+    card.getBody().innerHTML = "<br>Prodejce Volkswagen";
+    card.setSize(270, 90);
+    card.getHeader().innerHTML = "<strong>" + data[i].name + "</strong>";
+    znacky[i].decorate(SMap.Marker.Feature.Card, card);
 }
+
+
 
 var cz = mapa.computeCenterZoom(souradnice); /* Spočítat pozici mapy tak, aby značky byly vidět */
 mapa.setCenterZoom(cz[0], cz[1]);
+var trasa;
+var vrstvaSTrasou = new SMap.Layer.Geometry();
+mapa.addLayer(vrstvaSTrasou).enable();
 
 var nalezeno = function (route) {
-    var vrstva = new SMap.Layer.Geometry();
-    mapa.addLayer(vrstva).enable();
-
     var coords = route.getResults().geometry;
     var cz = mapa.computeCenterZoom(coords);
-    mapa.setCenterZoom(cz[0], cz[1]);
-    var g = new SMap.Geometry(SMap.GEOMETRY_POLYLINE, null, coords);
-    vrstva.addGeometry(g);
+    //mapa.setCenterZoom(cz[0], cz[1]);
+    if (trasa != null) {
+        vrstvaSTrasou.removeGeometry(trasa);
+    }
+    trasa = new SMap.Geometry(SMap.GEOMETRY_POLYLINE, null, coords);
+    vrstvaSTrasou.addGeometry(trasa);
 }
 
-var coords = [
-    SMap.Coords.fromWGS84(14.3579142, 50.0479742),
-    SMap.Coords.fromWGS84(14.4917869, 50.1320075)
-];
-var route = new SMap.Route(coords, nalezeno);
+var markerClicked = false;
+var endPointCoords;
+
+function handleMarkerClick(e) { /* Kliknutí na marker */
+    markerClicked = true;
+    // Vybraný marker
+    var marker = e.target;
+    var id = marker.getId();
+    loadDoc(id);
+    // Párovaní vybraného markeru pomocí jeho ID a našich vstupních dat.
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
+            endPointCoords = SMap.Coords.fromWGS84(data[i].coords);
+            break;
+        }
+    }
+    var startPointCoords;
+    if (startovniBod != null) {
+        // Máme startovní bod, najdeme trasu.
+        startPointCoords = startovniBod._coords;
+    } else {
+        // Nemáme startovní bod, najdeme trasu ze středu mapy a přidáme tam marker.
+        startPointCoords = SMap.Coords.fromWGS84(14.3573103, 50.0479011);
+        var options = {
+            url: obrazek_start,
+            title: "Startovní bod",
+            anchor: { left: 10, bottom: 1 }  /* Ukotvení značky za bod uprostřed dole */
+        };
+        startovniBod = new SMap.Marker(startPointCoords, null, options);
+        startovniBod.decorate(SMap.Marker.Feature.Draggable);
+        vrstva.addMarker(startovniBod);
+    }
+    var coords = [startPointCoords, endPointCoords];
+    var route = new SMap.Route(coords, nalezeno);
+}
+
+function handleMapClick(e) { /* Kliknutí do mapy */
+    // Ten if je tady aby se neregistrovaly kliknutí na markery jako kliknutí do
+    // mapy. Kdyby tady ten if nebyl, tak by každé kliknutí na marker zároveň bylo
+    // registrováno jako kliknutí do mapy a přidalo by to výchozí bod někam k
+    // markeru, na který se kliknulo (a to nechceme).
+    if (markerClicked) {
+        // Uživatel kliknul na marker. Nechceme přidat výchozí bod na mapu, ale
+        // aspoň nastavíme atribut markerClicked na false, aby při příštím kliknutí
+        // na mapu byl vybrán bod na mapě. Když příště uživatel zase klikne na
+        // marker, žádný bod se na mapu zase nepřidá.
+        markerClicked = false;
+    } else {
+        // Souřadnice bodu kam se kliknulo
+        var startPointCoords = SMap.Coords.fromEvent(e.data.event, mapa);
+        if (startovniBod != null) {
+            // Odstraníme starý startovní bod
+            vrstva.removeMarker(startovniBod);
+        }
+        var options = {
+            url: obrazek_start,
+            title: "Startovní bod",
+            anchor: { left: 10, bottom: 1 }  /* Ukotvení značky za bod uprostřed dole */
+        };
+        startovniBod = new SMap.Marker(startPointCoords, null, options);
+        startovniBod.decorate(SMap.Marker.Feature.Draggable);
+        vrstva.addMarker(startovniBod);
+        if (trasa != null) {
+            // Už jsme dřív našli nějakou trasu, tak ji teď přepočítáme pro nový
+            // startovní bod.
+            var coords = [startPointCoords, endPointCoords];
+            var route = new SMap.Route(coords, nalezeno);
+        }
+    }
+}
+
+function start(e) { /* Začátek tažení */
+    var node = e.target.getContainer();
+    node[SMap.LAYER_MARKER].style.cursor = "default";
+}
+
+function stop(e) { /* Konec tažení */
+    var node = e.target.getContainer();
+    node[SMap.LAYER_MARKER].style.cursor = "pointer";
+    var startPointCoords = e.target.getCoords();
+    if (trasa != null) {
+        var coords = [startPointCoords, endPointCoords];
+        var route = new SMap.Route(coords, nalezeno);
+    }
+}
+
+var signals = mapa.getSignals();
+signals.addListener(window, "marker-drag-stop", stop);
+signals.addListener(window, "marker-drag-start", start);
+signals.addListener(this, "marker-click", handleMarkerClick);
+signals.addListener(this, "map-click", handleMapClick);
+
+function loadDoc(id) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) { // DONE and Success
+            var jsonResponse = JSON.parse(this.responseText);
+            jsonResponse.forEach(function (marker) {
+                if (marker.id == id) {
+                    document.getElementById("ajax").innerHTML = marker.name + ", popis: " + marker.popis;
+                }
+            });
+        }
+    };
+    xhttp.open("GET", "txt/data.txt", true);
+    xhttp.send();
+}
