751dc64ad7acc1578fe32f580fc036486ba99e98
ujhelluk <ujhelluk@fel.cvut.cz>
2019-01-18 13:48:47 +0100

    axios linked, getMovies function test with axios request to the api

--- www/ujhl00/project2/js/main.js
+++ www/ujhl00/project2/js/main.js
@@ -1,12 +1,22 @@
+//Zachytenie vstupu z formulára, získam hodnotu a volám funkciu getMovies()
 $(document).ready(function () {
     $('#searchForm').on('submit', function (e) {
-        console.log($('#searchText').val());
-        //let searchText = $('#searchText').val();
+        //console.log($('#searchText').val());
+        let searchText = $('#searchText').val();
         getMovies(searchText);
         e.preventDefault();
     });
 });
 
 function getMovies(searchText) {
+    //console.log(searchText);
 
+    //axios api request
+    axios.get('http://www.omdbapi.com?s=' + searchText + "&apikey=d8ae8847")
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {  //v prípade nejakého error
+            console.log(err)    //výpis do console
+        });
 }
