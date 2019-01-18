b52b16904c6a05ce35a949ce630103a10069e981
ujhelluk <ujhelluk@fel.cvut.cz>
2019-01-18 18:09:28 +0100

    getMovie output completed

--- www/ujhl00/project2/js/main.js
+++ www/ujhl00/project2/js/main.js
@@ -25,7 +25,8 @@ function getMovies(searchText) {
                     <div class="well text-center">
                         Žiadny film nebol nájdený! Skúste vyhľadávanie znovu...
                     </div>
-                </div>`;
+                </div>
+                `;
             } else {
                 // each loop poľa, pripojenie každého filmu do premennej output a jej výpis
                 $.each(movies, function (index, movie) {
@@ -38,7 +39,8 @@ function getMovies(searchText) {
                             <br>
                             <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Detail filmu</a>
                         </div>
-                    </div>`;
+                    </div> 
+                    `;
                 });
             }
 
@@ -72,9 +74,29 @@ function getMovie() {
                         <img src="${movie.Poster}" class="thumbnail">
                     </div>
                     <div class="col-md-8">
-                 
+                        <h2>${movie.Title}</h2>
+                            <ul class="list-group">
+                                <li class="list-group-item">Žáner: ${movie.Genre}</li>
+                                <li class="list-group-item">Vydané: ${movie.Released}</li>
+                                <li class="list-group-item">Obsadenie: ${movie.Actors}</li>
+                                <li class="list-group-item">Produkcia: ${movie.Production}</li>
+                                <li class="list-group-item">Režisér: ${movie.Director}</li>
+                                <li class="list-group-item">Autor: ${movie.Writer}</li>
+                                <li class="list-group-item">Dĺžka: ${movie.Runtime}</li>
+                                <li class="list-group-item">IMDB hodnotenie: ${movie.imdbRating} / 10</li>
                     </div>
-                </div> `;
+                </div> 
+                <div class="row">
+                    <div class="well">
+                        <br><br><hr>
+                        <h3>Obsah</h3>
+                        ${movie.Plot}
+                        <br><br><br>
+                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">Prejsť na IMDB</a>
+                        <a href="index.html" class="btn btn-primary">Späť na vyhľadávanie</a>
+                    </div>  
+                </div>
+                `;
 
             $('#movie').html(output);
             $(document).attr("title", "Filmová databáza | " + movie.Title);
