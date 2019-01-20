04dea330ebdacfba4db29b475a8f5419bd2ac247
Arkatos1 <arkatos1234@gmail.com>
2019-01-20 19:32:06 +0100

    Pridan posledni tweet smerujici na uzivatele, pridany dalsi kategorie, pridana tabulka, upravena grafika

--- www/dohp02/SP2/js/app.js
+++ www/dohp02/SP2/js/app.js
@@ -1,6 +1,5 @@
 var cb = new Codebird;
 cb.setConsumerKey("7T3WDEIZI6AlJzuuPrufryPJk", "ChNjwMAe75zthLay6dwK5kpDqptuY6j3n9eEUF2WhHs9yy2qrX");
-
 cb.__call("oauth2_token", {}, function (reply, err) {
     var bearer_token;
     if (err) {
@@ -15,28 +14,55 @@ function searchUsers() {
         screen_name: document.getElementById("search-input").value
     };
     document.getElementById("latest_container").innerHTML = "";
+    document.getElementById("first_container").innerHTML = "";
+    document.getElementById("latestdirected_container").innerHTML = "";
     cb.__call(
         "users_show",
         params,
         function (reply, rate, err) {
             console.log(reply);
-            document.getElementById("name").innerHTML = "Name: " + reply.name;
-            document.getElementById("description").innerHTML = "Description: " + reply.description;
-            document.getElementById("location").innerHTML = "Location: " + reply.location;
-            document.getElementById("created").innerHTML = "Account created: " + reply.created_at;
-            document.getElementById("followers").innerHTML = "Number of followers: " + reply.followers_count;
-            document.getElementById("friends").innerHTML = "Number of followings: " + reply.friends_count;
-            document.getElementById("favourites").innerHTML = "Number of favourites: " + reply.favourites_count;
-            document.getElementById("statuses").innerHTML = "Number of tweets: " + reply.statuses_count;
-            document.getElementById("verified").innerHTML = "Verified: " + reply.verified;
-
+            document.getElementById("profile_image").innerHTML = "<img src=" + reply.profile_image_url + ">";
+            document.getElementById("name").innerHTML = reply.name;
+            document.getElementById("description").innerHTML = reply.description;
+            document.getElementById("location").innerHTML = reply.location;
+            document.getElementById("created").innerHTML = reply.created_at;
+            document.getElementById("followers").innerHTML = reply.followers_count;
+            document.getElementById("friends").innerHTML = reply.friends_count;
+            document.getElementById("favourites").innerHTML = reply.favourites_count;
+            document.getElementById("statuses").innerHTML = reply.statuses_count;
+            document.getElementById("verified").innerHTML = reply.verified;
             twttr.widgets.createTweet(
                 reply.status.id_str,
                 document.getElementById('latest_container'),
                 {
                     theme: 'light',
-                    conversation: 'no'
                 }
+            );
+            twttr.widgets.createTweet(
+                '0',
+                document.getElementById('first_container'),
+                {
+                    theme: 'light',
+                }
+            );
+            var paramsTweets = {
+                q: "to:" + reply.screen_name,
+                count: 1
+            };
+            cb.__call(
+                "search_tweets",
+                paramsTweets,
+                function (reply) {
+                    console.log(reply);
+                    twttr.widgets.createTweet(
+                        reply.statuses[0].id_str,
+                        document.getElementById('latestdirected_container'),
+                        {
+                            theme: 'light',
+                        }
+                    );
+                },
+                true
             );
         },
         true
