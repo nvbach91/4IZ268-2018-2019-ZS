b15e78a9601ab7b6d1748b315f434d632f41cf38
Arkatos1 <arkatos1234@gmail.com>
2019-01-20 23:41:25 +0100

    pridan nahodny tweet od uzivatele, zpracovan datum do citelne podoby, filtrace retweetu, mensi uprava layoutu, validni html5

--- www/dohp02/SP2/js/app.js
+++ www/dohp02/SP2/js/app.js
@@ -21,29 +21,64 @@ function searchUsers() {
         params,
         function (reply, rate, err) {
             console.log(reply);
+
+            var split = reply.created_at.split(' ');
+            var date = split.splice(1, 2);
+            var year = split[split.length - 1];
+            date.push(year);
+            date = date.join(' ');
+
             document.getElementById("profile_image").innerHTML = "<img src=" + reply.profile_image_url + ">";
             document.getElementById("name").innerHTML = reply.name;
             document.getElementById("description").innerHTML = reply.description;
             document.getElementById("location").innerHTML = reply.location;
-            document.getElementById("created").innerHTML = reply.created_at;
+            document.getElementById("created").innerHTML = date;
             document.getElementById("followers").innerHTML = reply.followers_count;
             document.getElementById("friends").innerHTML = reply.friends_count;
             document.getElementById("favourites").innerHTML = reply.favourites_count;
             document.getElementById("statuses").innerHTML = reply.statuses_count;
             document.getElementById("verified").innerHTML = reply.verified;
-            twttr.widgets.createTweet(
-                reply.status.id_str,
-                document.getElementById('latest_container'),
-                {
-                    theme: 'light',
-                }
+            var Tweets = {
+                q: "-filter:nativeretweets from:" + reply.screen_name,
+                count: 1
+            };
+            cb.__call(
+                "search_tweets",
+                Tweets,
+                function (reply) {
+                    console.log(reply);
+                    twttr.widgets.createTweet(
+                        reply.statuses[0].id_str,
+                        document.getElementById('latest_container'),
+                        {
+                            theme: 'light',
+                        }
+                    );
+                },
+                true
             );
-            twttr.widgets.createTweet(
-                '0',
-                document.getElementById('first_container'),
-                {
-                    theme: 'light',
-                }
+
+            var min = 0;
+            var max = 99;
+            var random = Math.floor(Math.random() * (+max - +min)) + +min;
+            var randomTweets = {
+                q: "-filter:nativeretweets from:" + reply.screen_name,
+                count: 100
+            };
+            cb.__call(
+                "search_tweets",
+                randomTweets,
+                function (reply) {
+                    console.log(reply);
+                    twttr.widgets.createTweet(
+                        reply.statuses[random].id_str,
+                        document.getElementById('first_container'),
+                        {
+                            theme: 'light',
+                        }
+                    );
+                },
+                true
             );
             var paramsTweets = {
                 q: "to:" + reply.screen_name,
