913e1c9f6de17ea73fa42b1ef29498f1b39a12d3
janf04 <janf04@vse.cz>
2019-01-21 22:43:17 +0100

    basics

--- www/janf04/semestralka2/js/main.js
+++ www/janf04/semestralka2/js/main.js
@@ -1,5 +1,28 @@
-$(document).ready(function () {
-    $("#btn").click(function () {
-        $("#form").load("data.txt");
+function searchVid() {
+    gapi.client.setApiKey('AIzaSyAgAcMNHu1LUVxiGKN8Onb_Qga3WTImnI8');
+    gapi.client.load('youtube', 'v3', function () {
+        makeRequest();
     });
-})
+}
+function makeRequest() {
+    var keyword = document.getElementById("keyword").value;
+    var searchRequest = gapi.client.youtube.search.list({
+        q: keyword,
+        part: 'snippet',
+        maxResults: 5
+    });
+    searchRequest.execute(function (response) {
+        var clearList = document.getElementById("searchResults");
+        while (clearList.firstChild) {
+            clearList.removeChild(clearList.firstChild);
+        }
+        var searchVids = response.result.items;
+
+        searchVids.forEach(function(item, index) {
+            var temp = document.getElementById("searchResults").innerHTML
+            document.getElementById("searchResults").innerHTML = temp + '<div class="video" >' + 
+            item.snippet.title + 
+            '<img src="' + item.snippet.thumbnails.high.url + '"></img>'
+        });
+    })
+}
