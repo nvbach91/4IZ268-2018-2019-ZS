8c22279c4ca6011df8625fcecff1aab4757b2a3e
Lukáš Hodboď <lukashodbod@Lukas-MacBook-Pro.local>
2019-01-22 00:03:14 +0100

    validace html, doplneni modal

--- www/hodl01/semestralka/js/main.js
+++ www/hodl01/semestralka/js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     var ptoken = '1404870044.fa0d083.290093ca2a4d4a7f90e28bf31ec3570d',
         token = ptoken,
         mtoken = '6320184047.209a707.58363885aaa94bcdb68332026275b3f1',
+        numphotos = 18,
         container = document.getElementById('instafeed'),
         modal = document.getElementById('myModal'),
-        modalImg = document.getElementById("img01"),
+        modalImg = document.getElementById("imgModal"),
         captionText = document.getElementById("caption"),
-        parsedResult,
-        requestArray = new Array("první hodnota", "druhá hodnota");
+        parsedResult;
 
     // vytvoření objektu XHR
     xhr = new XMLHttpRequest();
@@ -20,35 +20,49 @@ $(document).ready(function () {
 
             // je návratový kód v pořádku?
             if (xhr.status === 200) {
-
-
-                console.log(xhr.responseText);
+                container.style.display = "flex";
                 parsedResult = JSON.parse(xhr.response);
                 console.log(parsedResult.data);
+                var innerString = "";
                 for (x in parsedResult.data) {
-                    console.log(parsedResult.data[x].likes.count);
 
+                    if (document.getElementById('p' + x) == null) {
+                        // toto nefunguje :?
+                        //$('<div/>', {
+                        //     class: 'photoBox'
+                        // }).appendTo('instafeed');
+                        // $('<img/>', {
+                        //     id: 'p' + x
+                        // }).appendTo('photoBox[3]');
+                        innerString += ' <div class="photoBox"><img id = "p' + x + '" alt = "Photo from Instagram"><div class="midle"><div class="textOnHover" id="l' + x + '"><img class="icon" src="IMG/heart-solid.svg" alt="heart icon"></div></div></div>';
+                    }
+
+                }
+                container.innerHTML += innerString;
+
+                for (x in parsedResult.data) {
                     var img = document.getElementById('p' + x);
-
                     img.src = parsedResult.data[x].images.standard_resolution.url;
                     img.srcset = parsedResult.data[x].images.thumbnail.url + ' 150w,' + parsedResult.data[x].images.low_resolution.url + ' 306w,' + parsedResult.data[x].images.standard_resolution.url + ' 612w';
                     img.onload = fillByImage(img);
+                    img.data = x;
                     img.onclick = function () { openModal(this) };
                     //img.addEventListener("click", openModal(x));
 
-
                     var textOnHover = document.getElementById('l' + x);
-                    textOnHover.innerHTML += parsedResult.data[x].likes.count;
+                    textOnHover.innerHTML = '<img class="icon" src="IMG/heart-solid.svg" alt="heart icon"></img>' + parsedResult.data[x].likes.count;
+
                 }
 
             } else {
-                console.log(xhr.responseText);
+                parsedResult = JSON.parse(xhr.response);
+                alert(parsedResult.meta.error_message);
             }
         }
     }
 
     // nastavení způsobu odeslání požadavku
-    xhr.open("GET", 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + 2);
+    xhr.open("GET", 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + numphotos);
 
     // odeslání požadavku
     xhr.send();
@@ -59,28 +73,28 @@ $(document).ready(function () {
     }
 
     $('#profilBT').click(function () {
+        container.style.display = "none";
+        xhr.abort();
         if (this.innerHTML == '@makacenkoteam') {
             token = mtoken;
             this.innerHTML = '@lukashodbod';
             $('.profileName').text('Instagram @makacenkoteam');
-            xhr.abort();
-            xhr.open("GET", 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + 2);
-            xhr.send();
+            xhr.open("GET", 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + numphotos);
+
         } else {
             token = ptoken;
             this.innerHTML = '@makacenkoteam';
             $('.profileName').text('Instagram @lukashodbod');
-            xhr.abort();
-            xhr.open("GET", 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + 2);
-            xhr.send();
+            xhr.open("GET", 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + numphotos);
+
         }
+        xhr.send();
     });
     function openModal(obr) {
-        console.log(obr);
-
         modal.style.display = "block";
         modalImg.src = obr.src;
-        captionText.innerHTML = obr.alt;
+        modalImg.srcset = obr.srcset;
+        captionText.innerHTML = parsedResult.data[obr.data].caption.text;
     }
 
 });
