33375696169111953a2943795b8875e8af68e31c
smoldiel <smoldiel@gmail.com>
2019-01-22 20:33:11 +0100

    Oprava chyby (jmeno uzivatele)

--- www/vase03/Chat-App_VSE/server/lib/db/dbClient.js
+++ www/vase03/Chat-App_VSE/server/lib/db/dbClient.js
@@ -3,7 +3,7 @@ var async = require('async');
 
 client = mysql.createConnection({
     host     : '91.240.85.69',
-    user     : 'chat',
+    user     : 'chat_user',
     password : '123456789a'
 });
 
