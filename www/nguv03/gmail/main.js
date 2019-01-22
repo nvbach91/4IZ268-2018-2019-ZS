82032e407d91bfa61c20900ff4e14d73d421ff7e
Nguyen Viet Bach <nvbach91@outlook.com>
2019-01-22 13:18:15 +0100

    added gmail sender example

---
+++ www/nguv03/gmail/main.js
@@ -0,0 +1,103 @@
+const App = {};
+
+App.CLIENT_ID = '210723404057-plq0cgmio9ttlg5vpvib3ovppbqt4dqd.apps.googleusercontent.com';
+App.API_KEY = 'AIzaSyBklnZIea-NFuupxxXDegXVPK4yHXXSwxE';
+App.DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'];
+App.SCOPES = 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send';
+
+App.initClient = () => {
+  const authorizeButton = $('#authorize_button');
+  const signoutButton = $('#signout_button');
+
+  const updateSigninStatus = (isSignedIn) => {
+    if (isSignedIn) {
+      authorizeButton.hide();
+      signoutButton.show();
+      App.fetchUserInfo();
+      //showProfile();
+    } else {
+      authorizeButton.show();
+      signoutButton.hide();
+    }
+  };
+
+  gapi.client.init({
+    apiKey: App.API_KEY,
+    clientId: App.CLIENT_ID,
+    discoveryDocs: App.DISCOVERY_DOCS,
+    scope: App.SCOPES,
+  }).then(
+    () => {
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.click(() => {
+        gapi.auth2.getAuthInstance().signIn();
+      });
+      signoutButton.click(() => {
+        gapi.auth2.getAuthInstance().signOut();
+      });
+    }, (err) => {
+      console.error(err);
+    }
+  );
+};
+
+App.warn = (msg) => {
+  App.modal.find('.modal-body p').text(msg);
+  App.modal.modal();
+};
+
+App.bindEmailForm = () => {
+  const messageForm = $('#message-form');
+  const recipientInput = messageForm.find('input[placeholder="RECIPIENT"]');
+  const subjectInput = messageForm.find('input[placeholder="SUBJECT"]');
+  const messageInput = messageForm.find('textarea[placeholder="MESSAGE"]');
+  const messageFormSubmitButton = messageForm.find('button');
+  const clearEmailForm = () => {
+    recipientInput.val('');
+    subjectInput.val('');
+    messageInput.val('');
+    messageFormSubmitButton.prop('disabled', false);
+  };
+  messageForm.submit((e) => {
+    e.preventDefault();
+    messageFormSubmitButton.prop('disabled', true);
+    const emailRaw = `To: ${recipientInput.val()}\r\nSubject: ${subjectInput.val()}\r\n\r\n${messageInput.val()}`;
+    //console.log(emailRaw);
+    const sendRequest = gapi.client.gmail.users.messages.send({
+      userId: 'me',
+      resource: { raw: btoa(emailRaw).replace(/\+/g, '-').replace(/\//g, '_') },
+    });
+    sendRequest.execute((e) => {
+      //console.log(e);
+      clearEmailForm();
+      App.warn('Email was sent');
+    });
+  });
+};
+
+App.fetchUserInfo = () => {
+  gapi.client.load('plus', 'v1', () => {
+    gapi.client.plus.people.get({ userId: 'me' }).execute((resp) => {
+      const messageForm = $('#message-form');
+      const displayName = resp.displayName;
+      const email = resp.emails[0].value;
+      messageForm.find('input[placeholder="USERNAME"]').val(displayName);
+      messageForm.find('input[placeholder="EMAIL"]').val(email);
+      messageForm.find('#user-info').show();
+    });
+  });
+};
+
+$(document).ready(() => {
+  $.ajaxSetup({ cache: true });
+  $.getScript('https://apis.google.com/js/api.js')
+    .done((script, textStatus) => {
+      gapi.load('client:auth2', App.initClient);
+    })
+    .fail((jqxhr, settings, exception) => {
+      console.error('Failed to load Google API scripts');
+    });
+  App.modal = $('#modal');
+  App.bindEmailForm();
+});
