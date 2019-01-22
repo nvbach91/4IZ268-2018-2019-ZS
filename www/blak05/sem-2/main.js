4f0cffc8598a3e4067d13aee79d5a531a3f9fa0c
potatocz <blazej.krystof@gmail.com>
2019-01-22 10:55:11 +0100

    Předěláno na dvě strany, řádkování hotovo, api v proměnnych

--- www/blak05/sem-2/main.js
+++ www/blak05/sem-2/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    var baseApiLyrics = 'https://orion.apiseeds.com/api/music/lyric/'; 
+    var apiKeyLyrics = '?apikey=cRomUgw9cay6obVEueVwhiNZTGQFlqBv9evFRe1Xx1frFDCqy7leXKmiEfEMfcID';
+    var baseApiChart = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=c0fd8b3e8900c8cb6ec278f750bcf6cb&format=json';
     loadChartAjax();
     submit.addEventListener('submit', function(e) {
  
@@ -10,33 +13,37 @@ $(document).ready(function(){
         $('#lyric').empty();    
         $("#loader").show();
  
-        if(artist ===''){
+        if(!artist){
             alertArtist();
-        } else if (song ===''){
+        } else if (!song){
             alertSong();
         } else {
         
             $.ajax({
-            url: 'https://orion.apiseeds.com/api/music/lyric/'+ artist + '/' + song + "?apikey=cRomUgw9cay6obVEueVwhiNZTGQFlqBv9evFRe1Xx1frFDCqy7leXKmiEfEMfcID",
+            url: baseApiLyrics+ artist + '/' + song + apiKeyLyrics,
             type: "GET",
             dataType: "json",
             error: function(data){
                 $("#loader").hide();
+                $("#error").hide();
                 var error404 = error(data);
                 $("#lyric").html(error404);
                 },
             success: function(data){
-                var lyrics = lyric(data);
- 
-                $("#lyric").html(lyrics);
                 $("#loader").hide();
+                $("#error").hide();
+                var lyrics = lyric(data);
+                $("#lyric").html(lyrics);
                 }
             });
         };
     });
  
     function lyric(data){
-        return  '<h1>' + data.result.artist.name + ' - ' + data.result.track.name + '</h1><p>' + data.result.track.text + '</p>';
+        return  '<h1>' + data.result.artist.name + ' - ' + data.result.track.name + '</h1><p class="text">' + nahrad(data.result.track.text) + '</p>';
+    };
+    function nahrad(str){
+        return str.replace(/\n/g, "<br>");
     };
     
     function error(data){
@@ -44,16 +51,18 @@ $(document).ready(function(){
     };
     
     function alertArtist(){
-        document.getElementById("error").innerHTML = '<div class="alert alert-danger" role="alert">CHYBÍ JMÉNO UMĚLCE!!</div>';
+        $("#loader").hide();
+        $('#error').html('<div class="alert alert-danger" role="alert">CHYBÍ JMÉNO UMĚLCE!!</div>');
     };
     
     function alertSong(){
-        document.getElementById("error").innerHTML = '<div class="alert alert-danger" role="alert">CHYBÍ JMÉNO PÍSNĚ!!</div>';
+        $("#loader").hide();
+        $('#error').html('<div class="alert alert-danger" role="alert">CHYBÍ JMÉNO PÍSNĚ!!</div>');
     };
   
     function loadChartAjax() {
         $.ajax({
-        url:"http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=c0fd8b3e8900c8cb6ec278f750bcf6cb&format=json",
+        url: baseApiChart,
         type:"GET",
         dataType: 'json',
         success: function(data) {
@@ -73,16 +82,17 @@ $(document).ready(function(){
             var day = addZero(datum.getDate());
             var month = addZero(datum.getMonth()+1);
             var year = addZero(datum.getFullYear());
-            return '<p class="logo2">Last.Fm Chart</p><hr class="line2"> <p class="description">- aktuální k ' + day + '. ' + month + '. ' + year + ' -</p><p class="full-chart">' +  '<b>1. </b>'+ data.tracks.track[0].artist.name + ' - <b>' + data.tracks.track[0].name + '</b> <br>' + 
-                        '<b>2. </b>'+ data.tracks.track[1].artist.name + ' - <b>' + data.tracks.track[1].name + '</b> <br>' +
-                        '<b>3. </b>'+ data.tracks.track[2].artist.name + ' - <b>' + data.tracks.track[2].name + '</b> <br>' +
-                        '<b>4. </b>'+ data.tracks.track[3].artist.name + ' - <b>' + data.tracks.track[3].name + '</b> <br>' +
-                        '<b>5. </b>'+ data.tracks.track[4].artist.name + ' - <b>' + data.tracks.track[4].name + '</b> <br>' +
-                        '<b>6. </b>'+ data.tracks.track[5].artist.name + ' - <b>' + data.tracks.track[5].name + '</b> <br>' +
-                        '<b>7. </b>'+ data.tracks.track[6].artist.name + ' - <b>' + data.tracks.track[6].name + '</b> <br>' +
-                        '<b>8. </b>'+ data.tracks.track[7].artist.name + ' - <b>' + data.tracks.track[7].name + '</b> <br>' + 
-                        '<b>9. </b>'+ data.tracks.track[8].artist.name + ' - <b>' + data.tracks.track[8].name + '</b> <br>' + 
-                        '<b>10. </b>'+ data.tracks.track[9].artist.name + ' - <b>' + data.tracks.track[9].name + '</b> <p>'; 
+            return '<p class="description">- aktuální k ' + day + '. ' + month + '. ' + year + ' -</p><p class="full-chart">' +  
+                        '<strong>1. </strong>'+ data.tracks.track[0].artist.name + ' - <strong>' + data.tracks.track[0].name + '</strong> <br>' + 
+                        '<strong>2. </strong>'+ data.tracks.track[1].artist.name + ' - <strong>' + data.tracks.track[1].name + '</strong> <br>' +
+                        '<strong>3. </strong>'+ data.tracks.track[2].artist.name + ' - <strong>' + data.tracks.track[2].name + '</strong> <br>' +
+                        '<strong>4. </strong>'+ data.tracks.track[3].artist.name + ' - <strong>' + data.tracks.track[3].name + '</strong> <br>' +
+                        '<strong>5. </strong>'+ data.tracks.track[4].artist.name + ' - <strong>' + data.tracks.track[4].name + '</strong> <br>' +
+                        '<strong>6. </strong>'+ data.tracks.track[5].artist.name + ' - <strong>' + data.tracks.track[5].name + '</strong> <br>' +
+                        '<strong>7. </strong>'+ data.tracks.track[6].artist.name + ' - <strong>' + data.tracks.track[6].name + '</strong> <br>' +
+                        '<strong>8. </strong>'+ data.tracks.track[7].artist.name + ' - <strong>' + data.tracks.track[7].name + '</strong> <br>' + 
+                        '<strong>9. </strong>'+ data.tracks.track[8].artist.name + ' - <strong>' + data.tracks.track[8].name + '</strong> <br>' + 
+                        '<strong>10. </strong>'+ data.tracks.track[9].artist.name + ' - <strong>' + data.tracks.track[9].name + '</strong> </p>'; 
             };
     
     };
