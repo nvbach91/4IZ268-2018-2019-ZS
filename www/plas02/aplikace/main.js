5c1a574639840127abb9ee541452e49c034f6129
sarrka <sarka.placha@email.cz>
2019-01-24 00:22:04 +0100

    kvíz se mi již zobrazuje až po přihlášení, obsahuje i slidy a kod k odeslání, odeslání se mi již povedlo, ale při dalším předěláváním už mi tato fce neunguje a já nemůžu přijít na to proč. U slidů se mi bohužel první otázka zobrazuje dvakrát a také nevím, jak to vyřešit. poté jsem si nahráala otázky i ny myjson a v kodu je zakomentované nahrání tohoto jsonu, bohužel ale nevím jak pak definovat části tohoto jsonu, v kodu používám question, což je částí toho balíku. Myslíte že by jste mě mohl navést na ty chyby, už si s tím od vaše posledního komentu  lámu hlavu a pořád jsem na to nepřišla.

--- www/plas02/aplikace/main.js
+++ www/plas02/aplikace/main.js
@@ -1,7 +1,75 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var sendButton = document.getElementById('send');
-var email = document.getElementById("email").value;
+
+var CLIENT_ID = '581875226236-rthd0p3vt1a2r6sln82gans4j60e2cqc.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDPAK042knhPYBFrvpjEoFaBrtZu-vL-mk';
+
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
+
+var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+// volání načtení auth2 library a API client library.
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+//Inicializuje API client library a nastaví sign-in stav
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function (error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+//Když se změní status, aplikace ke změně tlačítek a volání fce printquiz
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        printquiz();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+
+
+var myQuestions;
+
+/**$.get('https://api.myjson.com/bins/6nif0', function (data, textStatus, jqXHR) {
+    myQuestions = JSON.stringify(data);
+    $("#data").val(myQuestions);
+    console.log(myQuestions);
+});*/
 
 
 var myQuestions = [
@@ -97,176 +165,162 @@ var myQuestions = [
     }
 ];
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, sendButton);
-
-function generateQuiz(questions, quizContainer, resultsContainer, sendButton) {
-
-    function showQuestions(questions, quizContainer) {
-        // místo pro otázky
-        var output = [];
-        var answers;
 
 
-        for (var i = 0; i < questions.length; i++) {
-            answers = [];
-            for (letter in questions[i].answers) {
 
-                // přidání možnosti výběru před odpovědi
-                answers.push(
-                    '<label>'
-                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-                    + letter + ': '
-                    + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
+function printquiz() {
 
-            // přidání naší otázky s odpovědmi do pole output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
+    var quizContainer = document.getElementById('quiz');
+    var resultsContainer = document.getElementById('results');
+    var sendButton = document.getElementById('send');
 
-        //uprava našich otázek na html string a přidání na stránku
-        quizContainer.innerHTML = output.join('');
-    }
+    generateQuiz(myQuestions, quizContainer, resultsContainer, sendButton);
+
+    function generateQuiz(questions, quizContainer, resultsContainer, sendButton) {
+
+        function showQuestions(questions, quizContainer) {
+            // místo pro otázky
+            var output = [];
+            var answers;
 
 
-    function showResults(questions, quizContainer, resultsContainer) {
+            for (var i = 0; i < questions.length; i++) {
+                answers = [];
+                for (letter in questions[i].answers) {
 
-        var answerContainers = quizContainer.querySelectorAll('.answers');
-
-        // proměné pro ukládání skóre
-        var userAnswer = '';
-        var points = 0;
-
-        for (var i = 0; i < questions.length; i++) {
-
-            // nalezení konkretní otázky a odpovědi
-            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-            // co se stane v případě správné odpovědi - načtou se body a změní barva
-            if (userAnswer === questions[i].correctAnswer) {
-                points++;
-                answerContainers[i].style.color = "green";
-            } else {
-                //v případě špatné odpovědi na červeno
-                answerContainers[i].style.color = "red";
-            }
-
-        }
-
-        //definice proměné vysledek, která bude definovat úroveň aj uživatele
-        var vysledek;
-        if (points <= 2) {
-            vysledek = "začátečník";
-        }
-        if (points > 2 && points <= 6) {
-            vysledek = "mírně pokročilý";
-        }
-        if (points > 6) {
-            vysledek = "pokročilý";
-        }
-
-        //vytisknutí výsledku
-        resultsContainer.innerHTML = 'Správně: ' + points + ". Tvoje úroveň angličtiny je " + vysledek + ".";
-    }
-
-    //pro zorbrazní otázek s odpovědmi
-    showQuestions(questions, quizContainer);
-
-    //fce při kliknutí na tlačítko
-    sendButton.onclick = function () {
-
-        showResults(questions, quizContainer, resultsContainer);
-
-        //definování vlastní client ID a API
-        var clientId = '581875226236-hhedtikokbr5ngbj4ud3r8g9f90nv4ho.apps.googleusercontent.com';
-        var apiKey = 'AIzaSyC-sI_Sq4BkTJxVh6GtpgyVh_LrHekt28c';
-
-        //nutné scopes, možná by šlo i bez labels a modify
-        var scopes = ['https://mail.google.com/', 'https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.modify', 'https://www.googleapis.com/auth/gmail.labels'];
-
-
-        function handleClientLoad() {
-            gapi.client.setApiKey(apiKey);
-            window.setTimeout(checkAuth, 1);
-        }
-
-        function checkAuth() {
-            gapi.auth.authorize({
-                client_id: clientId,
-                scope: scopes,
-                immediate: true
-            }, handleAuthResult);
-        }
-
-        function handleAuthResult(authResult) {
-            if (authResult && !authResult.error) {
-                loadGmailApi();
-            }
-        }
-
-        function loadGmailApi() {
-            gapi.client.load('gmail', 'v1', function () {
-                console.log("Loaded GMail API");
-            });
-        }
-
-
-        //fce na odeslání emailu s načtením příjemce z formuláře
-        sendEmail = function () {
-            var content = 'Správně: ';
-            var sender = 'salulinka@gmail.com';
-            var receiver = 'sarka.placha@email.cz';
-            var to = 'To: ' + receiver;
-            var from = 'From: ' + sender;
-            var subject = 'Subject: ' + 'Vysledky testu';
-            var contentType = 'Content-Type: text/plain; charset=utf-8';
-            var mime = 'MIME-Version: 1.0';
-
-            var message = "";
-            message += to + "\r\n";
-            message += from + "\r\n";
-            message += subject + "\r\n";
-            message += contentType + "\r\n";
-            message += mime + "\r\n";
-            message += "\r\n" + content;
-
-            sendMessage(message, receiver, sender);
-            console.log("sendEmail");
-        }();
-
-        function sendMessage(message, receiver, sender) {
-            var headers = getClientRequestHeaders();
-            var path = "http://wwww.gmail/v1/users/me/messages/send?key=" + clientId;
-            console.log("sendMessage_middle");
-            gapi.client.request({
-                path: path,
-                method: "POST",
-                headers: headers,
-                body: {
-                    'raw': window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
+                    // přidání možnosti výběru před odpovědi
+                    answers.push(
+                        '<label>'
+                        + '<input type="radio" name="question' + i + '" value="' + letter + '">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                        + '</label>'
+                    );
                 }
-            }).then(function (response) {
 
+                // přidání naší otázky s odpovědmi do pole output
+                output.push(
+                    '<div class="slide"> ' +
+                    '<div class="question">' + questions[i].question + '</div>'
+                    + '<div class="answers">' + answers.join('') + '</div>' +
+                    '<div class="question">' + questions[i++].question + '</div>'
+                    + '<div class="answers">' + answers.join('') + '</div>' +
+                    '<div class="question">' + questions[i++].question + '</div>'
+                    + '<div class="answers">' + answers.join('') + '</div>' +
+                    '<div class="question">' + questions[i++].question + '</div>'
+                    + '<div class="answers">' + answers.join('') + '</div>' +
+                    '<div class="question">' + questions[i++].question + '</div>'
+                    + '<div class="answers">' + answers.join('') + '</div>' +
+                    '</div> '
+                );
+
+            }
+
+            //uprava našich otázek na html string a přidání na stránku
+            quizContainer.innerHTML = output.join('');
+        }
+
+        function showResults(questions, quizContainer, resultsContainer) {
+
+            var answerContainers = quizContainer.querySelectorAll('.answers');
+
+            // proměné pro ukládání skóre
+            var userAnswer = '';
+            var points = 0;
+
+            for (var i = 0; i < questions.length; i++) {
+
+                // nalezení konkretní otázky a odpovědi
+                userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+                // co se stane v případě správné odpovědi - načtou se body a změní barva
+                if (userAnswer === questions[i].correctAnswer) {
+                    points++;
+                    answerContainers[i].style.color = "green";
+                } else {
+                    //v případě špatné odpovědi na červeno
+                    answerContainers[i].style.color = "red";
+                }
+
+            }
+
+            //definice proměné vysledek, která bude definovat úroveň aj uživatele
+            var vysledek;
+            if (points <= 2) {
+                vysledek = "začátečník";
+            }
+            if (points > 2 && points <= 6) {
+                vysledek = "mírně pokročilý";
+            }
+            if (points > 6) {
+                vysledek = "pokročilý";
+            }
+
+            //vytisknutí výsledku
+            resultsContainer.innerHTML = 'Správně: ' + points + ". Tvoje úroveň angličtiny je " + vysledek + ".";
+        }
+        //pro zorbrazní otázek s odpovědmi
+        showQuestions(questions, quizContainer);
+
+
+        function showSlide(n) {
+            slides[currentSlide].classList.remove("active-slide");
+            slides[n].classList.add("active-slide");
+            currentSlide = n;
+
+            if (currentSlide === 0) {
+                previousButton.style.display = "none";
+            } else {
+                previousButton.style.display = "inline-block";
+            }
+
+            if (currentSlide === slides.length - 1) {
+                nextButton.style.display = "none";
+                sendButton.style.display = "inline-block";
+            } else {
+                nextButton.style.display = "inline-block";
+                sendButton.style.display = "none";
+            }
+        }
+
+        function showNextSlide() {
+            showSlide(currentSlide + 1);
+        }
+
+        function showPreviousSlide() {
+            showSlide(currentSlide - 1);
+        }
+
+        const previousButton = document.getElementById("previous");
+        previousButton.style.display = "block"
+        const nextButton = document.getElementById("next");
+        nextButton.style.display = "block"
+        const slides = document.querySelectorAll(".slide");
+        let currentSlide = 0;
+
+        showSlide(0);
+
+
+        previousButton.addEventListener("click", showPreviousSlide);
+        nextButton.addEventListener("click", showNextSlide);
+
+        //fce při kliknutí na tlačítko
+        sendButton.onclick = function () {
+
+            showResults(questions, quizContainer, resultsContainer);
+            var name = document.getElementById("name").value;
+            var age = document.getElementById("age").value;
+            var body = name + '\n' + age;
+
+            var infoEmail = 'To: sarka.placha@email.cz\r\nSubject: Nový uživatelz\r\n\r\n' + body;
+
+            const sendRequest = gapi.client.gmail.users.messages.send({
+                'userId': 'me',
+                'resource': {
+                    'raw': btoa(unescape(encodeURIComponent(infoEmail))).replace(/\+/g, '-').replace(/\//g, '_')
+                }
             });
-            console.log("sendMessage");
-        }
-
-
-        var t = ' ';
-        function getClientRequestHeaders() {
-            if (!t) t = gapi.auth.getToken();
-            gapi.auth.setToken({ token: ['access_token'] });
-            var a = "Bearer " + ["access_token"];
-            return {
-                "Authorization": a,
-                "X-JavaScript-User-Agent": "Google APIs Explorer"
-            };
 
         }
-        console.log("odeslání zprávy");
     }
 }
