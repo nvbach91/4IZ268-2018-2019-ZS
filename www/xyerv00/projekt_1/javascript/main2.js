44e01f85bd7bce2929fb44d73f6ec4af0f8aa4de
Veronika <nika.yer@seznam.cz>
2019-01-23 17:38:06 +0100

    predelani pridavani polozek objednavky zatim bez stylovani, ale funkcni

--- www/xyerv00/projekt_1/javascript/main2.js
+++ www/xyerv00/projekt_1/javascript/main2.js
@@ -1,8 +1,55 @@
 var App = App || {};
 
 var itemID = 2;
-var item = $(".items").html();
+//var item = $(".items").html();
+var itemSelect = '';
 var mailTo = 'vyvojar.pat@gmail.com';
+
+var products = [
+    {
+        name: "Mika",
+        sizes:[36, 38, 40, 42]
+    },
+    {
+        name: "Viktoria",
+        sizes: [36, 38, 40, 42]
+    },
+    {
+        name: "Fiji",
+        sizes: [36, 38, 40, 42]
+    }
+    ]
+;
+
+function generateModelButtons() {
+    var itemButton = '<div class="modelSelectButtons">';
+    for (var i=0; i < products.length; i++){
+        itemButton += '<button name="model">'+products[i].name+'</button>';
+    }
+    itemButton +='</div>';
+    return  itemButton;
+}
+
+function generateOrderItem(modelOrederItem) {
+    var orderItem = '';
+    orderItem += '<div class="item"><label>' + modelOrederItem + '</label><select name="size">'+generateSizeOptions(modelOrederItem)+'</select><input type="number" name="count"></div>';
+    return orderItem;
+}
+
+function  generateSizeOptions(modelOrederItem){
+    var modelSizeOptions = [];
+    var sizeOptions = '';
+    for (var i=0; i < products.length; i++){
+        if (modelOrederItem === products[i].name){
+            for (var u =0; u < products[i].sizes.length; u++) {
+                sizeOptions += '<option>'+products[i].sizes[u]+'</option>';
+            }
+    }
+    }
+    return sizeOptions;
+}
+
+
 
 function orderContent(){
     var items = [];
@@ -11,9 +58,9 @@ function orderContent(){
         var item = $(this);
 
         items.push({
-            model: item.children("select[name='model']").val(),
+            model: item.children("label").text(),
             size: item.children("select[name='size']").val(),
-            count:item.children("select[name='count']").val()
+            count:item.children("input[name='count']").val()
         });
 
     });
@@ -63,10 +110,17 @@ App.init = function(){
 $(document).ready(function () {
     App.init();
 
+    $(".modelButtons").append(generateModelButtons());
+
     $("#buttonAdd").click(function(){
         $(".items").append(item);
         $(".item").last().attr("id", itemID);
         itemID++;
+    });
+
+    $("button[name='model']").click(function () {
+        var modelOrederItem = $(this).text();
+        $(".items").append(generateOrderItem(modelOrederItem));
     });
 
     $("#buttonDelete").click(function () {
