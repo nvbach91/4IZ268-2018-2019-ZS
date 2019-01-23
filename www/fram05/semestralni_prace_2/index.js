b555cd808e86fe2a052696186aece32079a3391a
Matyas Frank <matyasfrank@Matyass-MacBook-Pro.local>
2019-01-23 01:30:27 +0100

    Mimo bootstrapu je vse upraveno podle komentaru

--- www/fram05/semestralni_prace_2/index.js
+++ www/fram05/semestralni_prace_2/index.js
@@ -1,55 +1,65 @@
-var checked_chars = "";
-function check_checks() {
-    var capitals_checked = "";
-    var small_checked = "";
-    var numbers_checked = "";
-    var others_checked = "";
+const charSets = {
+    "lowercase": "abcdefghijklmnopqrstuvwxyz",
+    "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "numbers": "1234567890",
+    "others": "@/\\()-*+#&"
+};
+const form = document.getElementById("password-generator");
+const submitButton = document.querySelector("#password-generator button[type='submit']");
+submitButton.focus();
+form.addEventListener("submit", function (ev) {
+    // Prevent sending of the form
+    ev.preventDefault();
 
-    if (document.form.capitals.checked == false && document.form.smalls.checked == false && document.form.numbers.checked == false && document.form.others.checked == false) {
-        window.alert("None of the parameters were selected. Please choose at least one to process the request!");
-    } else {
-        if (document.form.capitals.checked == true) {
-            capitals_checked = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        } else {
-            capitals_checked = "";
+    const passwordLength = document.getElementById("password-length").value;
+
+    // get all option inputs
+    const options = document.querySelectorAll(".input-box input");
+    var passwordCharsSet = "";
+
+    // if they are selected, add charset by input name to final set
+    options.forEach(option => {
+        if (option.checked) {
+            passwordCharsSet += charSets[option.getAttribute("value")]
         }
-        if (document.form.smalls.checked == true) {
-            small_checked = "abcdefghijklmnopqrstuvwxyz";
-        } else {
-            small_checked = "";
-        }
-        if (document.form.numbers.checked == true) {
-            numbers_checked = "123456789";
-        } else {
-            numbers_checked = "";
-        }
-        if (document.form.others.checked == true) {
-            others_checked = "@/\()-*+#&";
-        } else {
-            others_checked = "";
-        }
+    });
+
+    // generate final password
+    const password = generatePassword(passwordCharsSet, passwordLength);
+
+    // render password to DOM
+    document.querySelector("#generated-password input").value = password;
+});
+
+function generatePassword(chars, length) {
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        password += chars[getRandomInt(chars.length)];
     }
-
-    checked_chars = capitals_checked.concat(small_checked, numbers_checked, others_checked);
-
-
-    document.getElementById("show").innerHTML = checked_chars;
+    return password;
 }
 
-function generate_pswd() {
-    var chars = document.getElementById("show").innerHTML;
-    var password = '';
-    for (var i = 0; i < 15; i++) {
-        var strng = Math.floor(Math.random() * chars.length);
-        password += chars.substring(strng, strng + 1);
-    }
-    document.password_box.blank.value = password;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 
 const input = document.getElementById("hash-input");
 const result = document.getElementById("hash-result");
 
-document.getElementById("hash-md5").addEventListener("click", ev => result.value = hex_md5(input.value));
-document.getElementById("hash-sha256").addEventListener("click", ev => result.value = hex_sha256(input.value));
-document.getElementById("hash-sha512").addEventListener("click", ev => result.value = hex_sha512(input.value));
+
+const md5 = new Hashes.MD5;
+const sha1 = new Hashes.SHA1;
+const sha256 = new Hashes.SHA256;
+const sha512 = new Hashes.SHA512;
+
+document.getElementById("hash-md5").addEventListener("click", ev => result.value = createHash(input.value, md5.hex));
+document.getElementById("hash-sha1").addEventListener("click", ev => result.value = createHash(input.value, sha1.hex));
+document.getElementById("hash-sha256").addEventListener("click", ev => result.value = createHash(input.value, sha256.hex));
+document.getElementById("hash-sha512").addEventListener("click", ev => result.value = createHash(input.value, sha512.hex));
+
+
+
+function createHash(text, hashFunction) {
+    return text != "" ? hashFunction(text) : "";
+}
