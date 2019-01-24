fa5aaeae19478a2fa6237ba8386086e5693b1ded
dsejna <david.sejna@seznam.cz>
2019-01-24 22:56:54 +0100

    SP2ver04

--- www/sejd00/web-app/js/auth.js
+++ www/sejd00/web-app/js/auth.js
@@ -1,11 +1,19 @@
-var urlLogin = `https://www.instagram.com/oauth/authorize/?client_id=62bd4a1225cc4f0097def241e69b2ad6&response_type=token&redirect_uri=http://127.0.0.1:5500/4IZ268-2018-2019-ZS/www/sejd00/web-app/index.html`;
 var tokenNew;
+var client_id = `62bd4a1225cc4f0097def241e69b2ad6`;
+var redirect_uri = `http://127.0.0.1:5500/4IZ268-2018-2019-ZS/www/sejd00/web-app/index.html`;
+//var redirect_uri = `https://fcp.vse.cz/4IZ268/2018-2019-ZS/www/sejd00/web-app/`;
 var hash = window.location.hash;
+var buttonVisibility = document.getElementById("logout");
+var token = localStorage.getItem("token");
+
+var urlLogin = `https://www.instagram.com/oauth/authorize/?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
+
 
 
 function authenticate() {
     window.open(urlLogin);
 }
+
 
 if (localStorage.getItem('token')) {
     tokenNew = localStorage.getItem('token');
@@ -17,7 +25,6 @@ if (hash) {
     if (cleanHash[0] === "access_token") {
         tokenNew = cleanHash[1]
     }
-
     localStorage.setItem('token', tokenNew)
 }
 
@@ -26,6 +33,10 @@ var logout = function () {
     $("body").append('<img id="img_logout" src="http://instagram.com/accounts/logout/" width="0" height="0" />');
     $("#img_logout").remove();
     localStorage.clear();
+    indicators.innerHTML = "";
+    posts.innerHTML = "";
+    label.innerHTML = "Uživatel odhlášen."
+    // window.open("index.html");
 };
 
 var auth = $('#auth').click(authenticate);
--- www/sejd00/web-app/js/hashtagApp.js
+++ www/sejd00/web-app/js/hashtagApp.js
@@ -1,13 +1,15 @@
-var searchButton = document.querySelector('#button');
+var searchButton = document.querySelector("#button");
 var input = document.querySelector("#search-input");
 var userProfile = document.querySelector("#user-profile");
 var label = document.querySelector("#photo");
 var likes = document.querySelector("#likes");
 var posts = document.querySelector("#posts");
-var indicators = document.querySelector("#seznam-indicators");
-var tag = document.querySelector("#text").value;;
-var pocet = 0;
-
+var indicators = document.querySelector("#list-indicators");
+var tag = document.querySelector("#hashtagSelector");
+var tagSearch;
+var indicatorNumber = 0;
+var a = [];
+var allTags;
 var token = localStorage.getItem("token");
 
 var fetchUsers = async () => {
@@ -17,40 +19,48 @@ var fetchUsers = async () => {
 }
 
 var showData = () => {
-    posts.innerHTML = `<div class="item active"><p class="post-intro">Prohližej šipkama!
-                      <div class="carousel-caption">
-                      <h3>Prohlížeč</h3>
-                      <p>20 obrázků</p>
-                      </div>
+    posts.innerHTML = `<div class="item active">
+                        <p class="post-intro">Prohlížej šipkama!</p>
                       </div>`;
     indicators.innerHTML = "";
     console.log(posts.innerHTML);
-    fetchUsers().then((res) => {
-        res.data.data.forEach(function (post) {
+    fetchUsers().then((respond) => {
+        label.innerHTML += `<p>Přihlášený uživatel: ${respond.data.data[0].user.full_name}</p>`;
+
+        respond.data.data.forEach(function (post) {
+
             post.tags.forEach(function (tg) {
-                if (tg === tag) {
-                    console.log(tg);
+
+                if (tg === tagSearch) {
                     // label.innerHTML += '<img class="image" src="' + post.images.standard_resolution.url + '" alt="First slide">';
                     // likes.innerHTML += '<p>Počet likes: ' + post.likes.count + '</p><br><p>' + post.caption.text + '</p>';
-                    indicators.innerHTML += `<li data-target="#myCarousel" data-slide-to="${pocet}"></li>`
-
-                    posts.innerHTML += `<div class="item"> <img src="${post.images.standard_resolution.url}" alt="Obrazek" style="width:50%;"> 
-                                        <div class="carousel-caption"> 
-                                        <h3>Fotka číslo: ${pocet + 1}</h3> 
-                                        <p>Počet likes: ${post.likes.count}</p>
-                                        </div>
+                    indicators.innerHTML += `<li data-target="#myCarousel" data-slide-to="${indicatorNumber}"></li>`;
+                    posts.innerHTML += `<div class="item"> <img src="${post.images.standard_resolution.url}" alt="Obrazek ${indicatorNumber + 1}">
+                                            <div class="desc"> 
+                                                <h3>Fotka číslo: ${indicatorNumber + 1}</h3> 
+                                                <p>Počet likes: ${post.likes.count}</p>
+                                                <p>Popis fotky: ${post.caption.text}</p>
+                                                <p><a href="#">#${post.tags[indicatorNumber]}</a></p>
+                                            </div>
                                        </div>`;
-                    pocet++;
+                    indicatorNumber++;
                 }
-            })
-        });;
-    }
-    );
-}
+            });
+        });
+    });
+};
 
 searchButton.addEventListener("click", (e) => {
-    posts.innerHTML = "";
-    tag = document.querySelector("#text").value;
-    e.preventDefault();
-    showData();
+    tagSearch = tag.value;
+    if (token === "undefined" || token === null || tagSearch === "") {
+        window.alert("Musíš zadat hashtag a být přihlášený.");
+        e.preventDefault();
+    } else {
+        indicatorNumber = 0;
+        posts.innerHTML = "";
+        label.innerHTML = "";
+        e.preventDefault();
+        showData();
+    }
+
 });
