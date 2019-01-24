9e2624a6a15dd2ccd3bc7096d4868962370f44f9
rinamoon <rinamoon007@gmail.com>
2019-01-24 11:05:06 +0100

    pridani api a jquery

--- www/khur00/SP2/main.js
+++ www/khur00/SP2/main.js
@@ -1,9 +1,42 @@
-$('.diary').hover(
-    function () {
-        $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
-    },
-    function () {
-        $('.description').fadeOut(50);
+
+/*var addToTest = document.getElementsByClassName('test1');
+var requestWeb = new XMLHttpRequest();
+requestWeb.open('GET', 'http://rinamogy.beget.tech/wp-json/wp/v2/posts/51');
+requestWeb.onload = function () {
+    if (requestWeb.status >= 200 && requestWeb.status < 400) {
+        var data = JSON.parse(this.response);
+        createHTML(data);
     }
-)
+    else {
+        console.log("We connected to a server ,but returned an error");
+    }
+
+}
+requestWeb.send();
+
+
+function createHTML(postData) {
+    var ourHTMLstring = '';
+    for (i = 0; i < postData.length; i++) {
+        ourHTMLstring += '<h1>' + postData[i].title.rendered + '/<h1>';
+
+    }
+    addToTest.innerHtml = ourHTMLstring;
+}
+
+$.getJSON('http://rinamogy.beget.tech/wp-json/wp/v2/posts/51', function (data) {
+
+    var text = `${data.title.rendered}<br>
+                ${data.contex.rendered}<br>`
+
+
+    $(".mypanel").html(text);
+});
+*/
+
+
+
+
+
+
+
