22e920c7f280e5b90aa1115b5c106864a50648ed
Matyas Frank <matyasfrank@Matyass-MacBook-Pro.local>
2019-01-30 08:03:06 +0100

    finalni verze

--- www/fram05/semestralni_prace_2/index.js
+++ www/fram05/semestralni_prace_2/index.js
@@ -1,7 +1,13 @@
+function copy() {
+    var copyText = document.getElementById("hash-result");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
 const charSets = {
-    "lowercase" : "abcdefghijklmnopqrstuvwxyz",
-    "uppercase" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "numbers" : "1234567890",
+    "lowercase": "abcdefghijklmnopqrstuvwxyz",
+    "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "numbers": "1234567890",
     "others": "@/\\()-*+#&"
 };
 const form = document.getElementById("password-generator");
@@ -12,7 +18,7 @@ form.addEventListener("submit", function (ev) {
     ev.preventDefault();
 
     const passwordLength = document.getElementById("password-length").value;
-   
+
 
     // get all option inputs
     const options = document.querySelectorAll(".input-box input");
@@ -20,7 +26,7 @@ form.addEventListener("submit", function (ev) {
 
     // if they are selected, add charset by input name to final set
     options.forEach(option => {
-        if(option.checked){
+        if (option.checked) {
             passwordCharsSet += charSets[option.getAttribute("value")]
         }
     });
@@ -39,13 +45,144 @@ form.addEventListener("submit", function (ev) {
     document.querySelector("#generated-password5 input").value = password5;
 });
 
-function generatePassword(chars, length)
-{
+function generatePassword(chars, length) {
     var password = "";
-    for(var i = 0; i < length; i++){
+    for (var i = 0; i < length; i++) {
         password += chars[getRandomInt(chars.length)];
     }
+
+
+    showMailModal();
+
+
+
     return password;
+}
+
+
+
+
+var clientId = '980441235767-inqc3re5c2f030nodoqpp9pbe0ork7s1.apps.googleusercontent.com';
+var apiKey = 'AIzaSyCvPbh_f_-J3Fto0awQ3o1d7-j6b-2zspc';
+var scopes =
+    ['https://www.googleapis.com/auth/gmail.send'];
+
+function handleAuthClick() {
+    gapi.auth.authorize({
+        client_id: clientId,
+        scope: scopes,
+        immediate: false
+    }, handleAuthResult);
+    return false;
+}
+
+function handleAuthResult(authResult) {
+    if (authResult && !authResult.error) {
+
+        gapi.client.load('gmail', 'v1', "").then(() => {
+
+
+            sendMessage({
+                'To': $("#mail-name").val(),
+                'Subject': 'HAHA'
+            }, "Your passwords are: " + createStringFromPasswords() + ".", "");
+            closeMailModal();
+        });
+    } else {
+        $(".mail-modal__container").children().remove();
+        $(".mail-modal__container").html("<button id='authorize-button'>Autorize</button>")
+        $('#authorize-button').removeClass("hidden");
+        $('#authorize-button').on('click', function () {
+            handleAuthClick();
+        });
+    }
+}
+
+
+function createStringFromPasswords() {
+    var string = "";
+    var counter = 0;
+    const inputs = document.querySelectorAll("#password-generator input[type='text']");
+    inputs.forEach(input => {
+        string += input.value;
+        counter++;
+        if (counter < inputs.length) string += ", ";
+    });
+    return string;
+}
+
+function sendMessage(headers_obj, message, callback) {
+    var email = '';
+    for (var header in headers_obj)
+        email += header += ": " + headers_obj[header] + "\r\n";
+    email += "\r\n" + message;
+    var sendRequest = gapi.client.gmail.users.messages.send({
+        'userId': 'me',
+        'resource': {
+            'raw': window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
+        }
+    });
+    return sendRequest.execute(callback);
+}
+
+
+function showMailModal() {
+    if (document.querySelector(".mail-modal")) return false;
+
+    const modal = document.createElement("DIV");
+    modal.classList.add("mail-modal");
+
+    const container = document.createElement("DIV");
+    container.classList.add("mail-modal__container");
+
+    const heading = document.createElement("P");
+    heading.innerText = "Would you like the passwords to be send to your e-mail adress?";
+    heading.classList.add("mail-modal__heading");
+
+    const yesButton = document.createElement("BUTTON");
+    yesButton.classList.add("button");
+    yesButton.setAttribute("id", "mail-yes");
+    yesButton.innerText = "YES";
+
+    yesButton.addEventListener("click", ev => {
+
+        $(".mail-modal__container").children().remove();
+        $(".mail-modal__container").html("<p>Please insert e-mail, where passwords should be sended:<input type='text' id='mail-name' /></p><button id='submit-mail-name'>Send</button>");
+
+        document.getElementById("submit-mail-name").addEventListener("click", ev => {
+            gapi.client.setApiKey(apiKey);
+            gapi.auth.authorize({
+                client_id: clientId,
+                scope: scopes,
+                immediate: true
+            }, handleAuthResult);
+        });
+
+
+    });
+
+    const noButton = document.createElement("BUTTON");
+    noButton.classList.add("button");
+    noButton.setAttribute("id", "mail-no");
+    noButton.innerText = "NO";
+    noButton.addEventListener("click", ev => {
+        closeMailModal();
+    });
+
+    container.appendChild(heading);
+    container.appendChild(noButton);
+    container.appendChild(yesButton);
+
+    modal.appendChild(container);
+
+
+    document.querySelector("body").appendChild(modal);
+}
+
+function closeMailModal() {
+    if (!document.querySelector(".mail-modal")) return false;
+
+    document.querySelector("body").removeChild(document.querySelector(".mail-modal"));
 }
 
 
@@ -67,8 +204,6 @@ document.getElementById("hash-md5").addEventListener("click", ev => result.value
 document.getElementById("hash-sha1").addEventListener("click", ev => result.value = createHash(input.value, sha1.hex));
 document.getElementById("hash-sha256").addEventListener("click", ev => result.value = createHash(input.value, sha256.hex));
 document.getElementById("hash-sha512").addEventListener("click", ev => result.value = createHash(input.value, sha512.hex));
-
-
 
 function createHash(text, hashFunction) {
     return text != "" ? hashFunction(text) : "";
