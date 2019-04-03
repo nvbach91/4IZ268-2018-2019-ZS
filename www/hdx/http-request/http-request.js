da6375f8be168baaa8ce4da8be9e0a08cf91de39
honza.do <honza.do@logio.cz>
2019-04-03 11:38:13 +0200

    url: getAllImg, removeImg, getImgByOpts

--- www/hdx/http-request/http-request.js
+++ www/hdx/http-request/http-request.js
@@ -3,34 +3,57 @@ var ENV = require('dotenv');
 ENV.config();
 
 const base = "https://" + process.env.CLOUD_KEY + ":" + process.env.CLOUD_SECRET + "@api.cloudinary.com/v1_1/" + process.env.CLOUD_NAME;
-const endpoints = {
-	getImages:"/resources/image",
-	getVideos:"/resources/video"
+
+function getAllImg(){
+    var url = base + "/resources/search?expression=resource_type:image";
+    
+    axios.post(url)
+    .then(res=>{
+        console.log("POST " + url);
+        console.log("total_count:" + res.data.total_count);
+        res.data.resources.forEach(function(image) {
+            console.log(image.public_id);
+        });
+    })
+    .catch(err=>console.log(err));
 }
-function getImages(options) {
+
+function getImgByOps(options, cursor) {
     if(!options)
         options = "";
-    var url = base + endpoints.getImages + options;
+    if(!cursor)
+        cursor = "";
+    var url = base + "/resources/image/upload?" + options + cursor;
+
     axios.get(url)
     .then(res=> 
         {
+            console.log("GET " + url);
             var nextCursor = res.data.next_cursor;
             res.data.resources.forEach(function(image) {
                 console.log(image.public_id);
             });
             if(nextCursor) {
-                getImages("/?next_cursor=" + nextCursor);
+                getImages(options, "&next_cursor=" + nextCursor);
             }
         })
     .catch(err=>console.log(err));
 }
 
-function removeImages() {
+function removeImg(publicIds) {
+    var url = base + "/resources/image/upload?";
+    publicIds.forEach(function (publicId) {
+        url += "&public_ids[]=" + publicId;
+    })
 
+    axios.delete(url)
+    .then(function(res) {
+        console.log("DELETE " + url);
+        console.log(res.data.deleted);
+    })
+    .catch(err=>console.log(err));;
 }
 
-function uploadImages() {
-
-}
-
-getImages();
+getAllImg();
+getImgByOps("prefix=art");
+removeImg(["hykiw77znszgb9kgcjm5", "vox_machina"]);
