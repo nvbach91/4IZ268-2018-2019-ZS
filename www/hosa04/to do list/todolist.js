3a552f20f620a88e41039ae7281034e985c15979
Adam Hospodka <43826633+adamhospodka@users.noreply.github.com>
2020-01-31 13:17:57 +0100

    Delete todolist.js

--- www/hosa04/to do list/todolist.js
+++
@@ -1,7 +0,0 @@
-function todoList(){
-    var item = document.getElementById("todoInput").value
-    var text = document.createTextNode(item)
-    var newItem = document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("todoList").appendChild(newItem)
-}
