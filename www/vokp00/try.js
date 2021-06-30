33417d281e0671dee581c76e39aeccd437f55488
petra08 <43857863+petra08@users.noreply.github.com>
2021-06-30 08:08:19 +0200

    Web dělejte vokp00
    Smazání obsahu složky vokp00

--- www/vokp00/try.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $.getJSON("//extreme-ip-lookup.com/json/",
-        function (json) {
-            if (json.country && json.city) {
-                $('#ipLookup').html('Location of ' + json.query + ': ' + json.city + ', ' + json.country + '');
-            }
-        }
-    );
-});
